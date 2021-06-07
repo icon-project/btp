6dd3c55a1f430091575fd46ae8b58bad8abeb7f7
Truong Ma Phi <trmaphi@gmail.com>
2021-06-07 22:31:24 +0700

    chore: remove unnecessary

--- chain/pra/events.go
+++ chain/pra/events.go
@@ -52,7 +52,6 @@ type EventMultiAccountUpdated struct {
 	Topics []types.Hash
 }
 
-// EventMultiAccountRemoved is emitted when a multi account has been removed. First param is the multisig account.
 type EventMultiAccountRemoved struct {
 	Phase  types.Phase
 	Who    types.AccountID
@@ -90,133 +89,15 @@ type EventMultisigCancelled struct {
 	Topics    []types.Hash
 }
 
-type EventFungibleTransfer struct {
-	Phase        types.Phase
-	Destination  types.U8
-	DepositNonce types.U64
-	ResourceId   types.Bytes32
-	Amount       types.U256
-	Recipient    types.Bytes
-	Topics       []types.Hash
-}
-
-type EventNonFungibleTransfer struct {
-	Phase        types.Phase
-	Destination  types.U8
-	DepositNonce types.U64
-	ResourceId   types.Bytes32
-	TokenId      types.Bytes
-	Recipient    types.Bytes
-	Metadata     types.Bytes
-	Topics       []types.Hash
-}
-
-type EventGenericTransfer struct {
-	Phase        types.Phase
-	Destination  types.U8
-	DepositNonce types.U64
-	ResourceId   types.Bytes32
-	Metadata     types.Bytes
-	Topics       []types.Hash
-}
-
-type EventRelayerThresholdChanged struct {
-	Phase     types.Phase
-	Threshold types.U32
-	Topics    []types.Hash
-}
-
-type EventChainWhitelisted struct {
-	Phase   types.Phase
-	ChainId types.U8
-	Topics  []types.Hash
-}
-
-type EventRelayerAdded struct {
-	Phase   types.Phase
-	Relayer types.AccountID
-	Topics  []types.Hash
-}
-
-type EventRelayerRemoved struct {
-	Phase   types.Phase
-	Relayer types.AccountID
-	Topics  []types.Hash
-}
-
-type EventVoteFor struct {
-	Phase        types.Phase
-	SourceId     types.U8
-	DepositNonce types.U64
-	Voter        types.AccountID
-	Topics       []types.Hash
-}
-
-type EventVoteAgainst struct {
-	Phase        types.Phase
-	SourceId     types.U8
-	DepositNonce types.U64
-	Voter        types.AccountID
-	Topics       []types.Hash
-}
-
-type EventProposalApproved struct {
-	Phase        types.Phase
-	SourceId     types.U8
-	DepositNonce types.U64
-	Topics       []types.Hash
-}
-
-type EventProposalRejected struct {
-	Phase        types.Phase
-	SourceId     types.U8
-	DepositNonce types.U64
-	Topics       []types.Hash
-}
-
-type EventProposalSucceeded struct {
-	Phase        types.Phase
-	SourceId     types.U8
-	DepositNonce types.U64
-	Topics       []types.Hash
-}
-
-type EventProposalFailed struct {
-	Phase        types.Phase
-	SourceId     types.U8
-	DepositNonce types.U64
-	Topics       []types.Hash
-}
-
 type SubstateWithFrontierEventRecord struct {
 	types.EventRecords
-	EVM_Log                             []EventEVMLog
-	Council_Proposed                    []types.EventCollectiveProposed       //nolint:stylecheck,golint
-	Council_Voted                       []types.EventCollectiveVoted          //nolint:stylecheck,golint
-	Council_Approved                    []types.EventCollectiveApproved       //nolint:stylecheck,golint
-	Council_Disapproved                 []types.EventCollectiveDisapproved    //nolint:stylecheck,golint
-	Council_Executed                    []types.EventCollectiveExecuted       //nolint:stylecheck,golint
-	Council_MemberExecuted              []types.EventCollectiveMemberExecuted //nolint:stylecheck,golint
-	Council_Closed                      []types.EventCollectiveClosed         //nolint:stylecheck,golint
-	ChainBridge_FungibleTransfer        []EventFungibleTransfer               //nolint:stylecheck,golint
-	ChainBridge_NonFungibleTransfer     []EventNonFungibleTransfer            //nolint:stylecheck,golint
-	ChainBridge_GenericTransfer         []EventGenericTransfer                //nolint:stylecheck,golint
-	ChainBridge_RelayerThresholdChanged []EventRelayerThresholdChanged        //nolint:stylecheck,golint
-	ChainBridge_ChainWhitelisted        []EventChainWhitelisted               //nolint:stylecheck,golint
-	ChainBridge_RelayerAdded            []EventRelayerAdded                   //nolint:stylecheck,golint
-	ChainBridge_RelayerRemoved          []EventRelayerRemoved                 //nolint:stylecheck,golint
-	ChainBridge_VoteFor                 []EventVoteFor                        //nolint:stylecheck,golint
-	ChainBridge_VoteAgainst             []EventVoteAgainst                    //nolint:stylecheck,golint
-	ChainBridge_ProposalApproved        []EventProposalApproved               //nolint:stylecheck,golint
-	ChainBridge_ProposalRejected        []EventProposalRejected               //nolint:stylecheck,golint
-	ChainBridge_ProposalSucceeded       []EventProposalSucceeded              //nolint:stylecheck,golint
-	ChainBridge_ProposalFailed          []EventProposalFailed                 //nolint:stylecheck,golint
-	MultiAccount_NewMultiAccount        []EventNewMultiAccount                //nolint:stylecheck,golint
-	MultiAccount_MultiAccountUpdated    []EventMultiAccountUpdated            //nolint:stylecheck,golint
-	MultiAccount_MultiAccountRemoved    []EventMultiAccountRemoved            //nolint:stylecheck,golint
-	MultiAccount_NewMultisig            []EventNewMultisig                    //nolint:stylecheck,golint
-	MultiAccount_MultisigApproval       []EventMultisigApproval               //nolint:stylecheck,golint
-	MultiAccount_MultisigExecuted       []EventMultisigExecuted               //nolint:stylecheck,golint
-	MultiAccount_MultisigCancelled      []EventMultisigCancelled              //nolint:stylecheck,golint
-	TreasuryReward_TreasuryMinting      []EventTreasuryMinting                //nolint:stylecheck,golint
+	EVM_Log                          []EventEVMLog
+	MultiAccount_NewMultiAccount     []EventNewMultiAccount     //nolint:stylecheck,golint
+	MultiAccount_MultiAccountUpdated []EventMultiAccountUpdated //nolint:stylecheck,golint
+	MultiAccount_MultiAccountRemoved []EventMultiAccountRemoved //nolint:stylecheck,golint
+	MultiAccount_NewMultisig         []EventNewMultisig         //nolint:stylecheck,golint
+	MultiAccount_MultisigApproval    []EventMultisigApproval    //nolint:stylecheck,golint
+	MultiAccount_MultisigExecuted    []EventMultisigExecuted    //nolint:stylecheck,golint
+	MultiAccount_MultisigCancelled   []EventMultisigCancelled   //nolint:stylecheck,golint
+	TreasuryReward_TreasuryMinting   []EventTreasuryMinting     //nolint:stylecheck,golint
 }
