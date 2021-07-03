94f436c2c6df94e962a8352675d9eacf246ef3c1
Kobby Pentangeli <kobby.pentangeli@hugobyte.com>
2021-07-03 04:55:14 +0000

    feat(bsh-generic): Init

---
+++ rust/near/bsh-generic/src/lib.rs
@@ -0,0 +1,56 @@
+/*
+#![forbid(
+    arithmetic_overflow,
+    mutable_transmutes,
+    no_mangle_const_items,
+    unknown_crate_types
+)]
+#![warn(
+    bad_style,
+    deprecated,
+    improper_ctypes,
+    missing_docs,
+    non_shorthand_field_patterns,
+    overflowing_literals,
+    stable_features,
+    unconditional_recursion,
+    unknown_lints,
+    unused,
+    unused_allocation,
+    unused_attributes,
+    unused_comparisons,
+    unused_features,
+    unused_parens,
+    unused_variables,
+    while_true,
+    clippy::unicode_not_nfc,
+    clippy::wrong_pub_self_convention,
+    clippy::unwrap_used,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    unused_results
+)]
+*/
+
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::{env, metadata, near_bindgen, setup_alloc};
+use std::collections::HashMap;
+
+setup_alloc!();
+
+// MOCK SETUP
+
+metadata! {
+    #[near_bindgen]
+    #[derive(BorshDeserialize, BorshSerialize)]
+    pub struct Token {
+        balances: HashMap<Vec<u8>, u64>,
+        allowances: HashMap<Vec<u8>, u64>,
+        pub owner: Vec<u8>,
+        pub ticker: String,
+        pub max_supply: u64,
+    }
+}
