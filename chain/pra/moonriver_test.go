1a9fa003983b42b8a9bbc0cc6b1245dd0d19bed6
Truong Ma Phi <trmaphi@gmail.com>
2021-07-06 09:56:59 +0700

    test: update to resolve unnecessary

--- chain/pra/moonriver_test.go
+++ chain/pra/moonriver_test.go
@@ -8,11 +8,12 @@ import (
 	gsrpc "github.com/centrifuge/go-substrate-rpc-client/v3"
 	types "github.com/centrifuge/go-substrate-rpc-client/v3/types"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMoonriverEventRecord(t *testing.T) {
 	api, err := gsrpc.NewSubstrateAPI("wss://wss.testnet.moonbeam.network")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	type test struct {
 		blockNumber      int
@@ -60,25 +61,25 @@ func TestMoonriverEventRecord(t *testing.T) {
 	}
 
 	meta, err := api.RPC.State.GetMetadataLatest()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	for _, test := range tests {
 		i := test.blockNumber
 
 		hash, err := api.RPC.Chain.GetBlockHash(uint64(i))
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		key, err := types.CreateStorageKey(meta, "System", "Events", nil, nil)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		storageRaw, err := api.RPC.State.GetStorageRaw(key, hash)
-		assert.NoError(t, err)
-
-		eventRecords := MoonriverEventRecord{}
-		err = types.EventRecordsRaw(*storageRaw).DecodeEventRecords(meta, &eventRecords)
-		assert.NoErrorf(t, err, "failed at %s", hash.Hex())
+		require.NoError(t, err)
 
 		assert.NotPanics(t, func() {
+			eventRecords := MoonriverEventRecord{}
+			err = types.EventRecordsRaw(*storageRaw).DecodeEventRecords(meta, &eventRecords)
+			assert.NoErrorf(t, err, "failed at %s", hash.Hex())
+
 			values := reflect.ValueOf(eventRecords)
 			for _, me := range test.moduleEventNames {
 				t.Run(fmt.Sprintf("should contains %s event", me), func(t *testing.T) {
