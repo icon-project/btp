7ef6d7870b4edacb3fead87ccf100df11ea7a269
Kobby Pentangeli <kobby.pentangeli@hugobyte.com>
2021-07-11 07:43:00 +0000

    Move unit tests into the module

--- rust/near/bsh-generic/src/bsh_generic.rs
+++ rust/near/bsh-generic/src/bsh_generic.rs
@@ -5,29 +5,30 @@ use crate::bsh_types::*;
 use btp_common::BTPAddress;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::{env, near_bindgen, setup_alloc};
+use near_sdk::{env, metadata, near_bindgen, setup_alloc};
 use std::collections::HashMap;
 
 setup_alloc!();
-
-/// BSH Generic contract is used to handle communications
-/// among BMC Service and a BSH core contract.
-/// This struct implements `Default`: https://github.com/near/near-sdk-rs#writing-rust-contract
-#[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize, Clone, Debug, Default, Deserialize, Serialize)]
-#[serde(crate = "near_sdk::serde")]
-pub struct BshGeneric {
-    bmc_contract: String,
-    bsh_contract: String,
-    /// A list of transferring requests
-    /// Use `HashMap` because `LookupMap` doesn't implement
-    /// Clone, Debug, and Default traits
-    requests: HashMap<u64, PendingTransferCoin>,
-    /// BSH Service name
-    service_name: String,
-    /// A counter of sequence number of service message
-    serial_no: u64,
-    num_of_pending_requests: u64,
+metadata! {
+    /// BSH Generic contract is used to handle communications
+    /// among BMC Service and a BSH core contract.
+    /// This struct implements `Default`: https://github.com/near/near-sdk-rs#writing-rust-contract
+    #[near_bindgen]
+    #[derive(BorshDeserialize, BorshSerialize, Clone, Debug, Default, Deserialize, Serialize)]
+    #[serde(crate = "near_sdk::serde")]
+    pub struct BshGeneric {
+        bmc_contract: String,
+        bsh_contract: String,
+        /// A list of transferring requests
+        /// Use `HashMap` because `LookupMap` doesn't implement
+        /// Clone, Debug, and Default traits
+        requests: HashMap<u64, PendingTransferCoin>,
+        /// BSH Service name
+        service_name: String,
+        /// A counter of sequence number of service message
+        serial_no: u64,
+        num_of_pending_requests: u64,
+    }
 }
 
 #[near_bindgen]
@@ -77,15 +78,15 @@ impl BshGeneric {
         let mut asset_details: Vec<AssetTransferDetail> = Vec::with_capacity(coin_names.len());
 
         for i in 0..coin_names.len() {
-            assets[i] = Asset {
+            assets.push(Asset {
                 coin_name: coin_names[i].clone(),
                 value: values[i],
-            };
-            asset_details[0] = AssetTransferDetail {
+            });
+            asset_details.push(AssetTransferDetail {
                 coin_name: coin_names[i].clone(),
                 value: values[i],
                 fee: fees[i],
-            };
+            });
         }
         // Send Service Message to BMC
         // BMC: bmc.send_message();
@@ -255,5 +256,139 @@ impl BshGeneric {
             // BSH core: bsh_core.transfer_fees(fa);
         }
         Ok(())
+    }
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use near_sdk::test_utils::VMContextBuilder;
+    use near_sdk::MockedBlockchain;
+    use near_sdk::{testing_env, VMContext};
+    use std::convert::TryInto;
+
+    fn get_context(is_view: bool) -> VMContext {
+        VMContextBuilder::new()
+            .signer_account_id("bob_near".try_into().unwrap())
+            .is_view(is_view)
+            .build()
+    }
+
+    #[test]
+    fn check_has_pending_request() {
+        let context = get_context(false);
+        testing_env!(context);
+        let bsh = BshGeneric::default();
+        let context = get_context(true);
+        testing_env!(context);
+        assert_eq!(bsh.has_pending_requests().unwrap(), false);
+    }
+
+    #[test]
+    fn check_send_service_message() {
+        let context = get_context(false);
+        testing_env!(context);
+        let mut bsh = BshGeneric::default();
+
+        let from = "btp://0x1.near/cx77ed9048b594b95199f326fc76e76a9d33dd665b";
+        let to = "btp://0x1.near/cx67ed9048b594b95199f326fc76e76a9d33dd665b";
+        let coin_names = vec!["btc".to_string(), "ether".to_string(), "usdt".to_string()];
+        let values = vec![100, 200, 300];
+        let fees = vec![1, 2, 3];
+
+        let context = get_context(true);
+        testing_env!(context);
+        assert!(bsh
+            .send_service_message(from, to, coin_names, values, fees)
+            .is_ok());
+    }
+
+    #[test]
+    fn check_handle_btp_message() {
+        let context = get_context(false);
+        testing_env!(context);
+        let mut bsh = BshGeneric::default();
+
+        let from = "btp://0x1.near/cx77ed9048b594b95199f326fc76e76a9d33dd665b";
+        let svc = "";
+        let sn = 1;
+        let msg = vec![b'1', b'2', b'3'];
+
+        let context = get_context(true);
+        testing_env!(context);
+        assert!(bsh.handle_btp_message(from, svc, sn, &msg).is_ok());
+    }
+
+    #[test]
+    fn check_handle_btp_error() {
+        let context = get_context(false);
+        testing_env!(context);
+        let mut bsh = BshGeneric::default();
+
+        let src = "btp://0x1.near/cx77ed9048b594b95199f326fc76e76a9d33dd665b";
+        let svc = "";
+        let sn = 1;
+        let code = 1;
+        let msg = "test-msg";
+
+        let context = get_context(true);
+        testing_env!(context);
+        assert!(bsh.handle_btp_error(src, svc, sn, code, msg).is_ok());
+    }
+
+    #[test]
+    fn check_handle_response_service() {
+        let context = get_context(false);
+        testing_env!(context);
+        let mut bsh = BshGeneric::default();
+
+        let sn = 1;
+        let code = 1;
+        let msg = "test-msg";
+
+        let context = get_context(true);
+        testing_env!(context);
+        assert!(bsh.handle_response_service(sn, code, msg).is_ok());
+    }
+
+    #[test]
+    fn check_handle_request_service() {
+        let context = get_context(false);
+        testing_env!(context);
+        let mut bsh = BshGeneric::default();
+
+        let btc = Asset {
+            coin_name: "btc".to_string(),
+            value: 100,
+        };
+        let ether = Asset {
+            coin_name: "ether".to_string(),
+            value: 200,
+        };
+        let usdt = Asset {
+            coin_name: "usdt".to_string(),
+            value: 300,
+        };
+        let assets = vec![btc, ether, usdt];
+        let to = "btp://0x1.near/cx67ed9048b594b95199f326fc76e76a9d33dd665b";
+
+        let context = get_context(true);
+        testing_env!(context);
+        assert!(bsh.handle_request_service(to, assets).is_ok());
+    }
+
+    #[test]
+    fn check_handle_fee_gathering() {
+        let context = get_context(false);
+        testing_env!(context);
+        let mut bsh = BshGeneric::default();
+
+        let fa = "btp://0x1.near/cx77ed9048b594b95199f326fc76e76a9d33dd665b";
+        let svc = "";
+
+        let context = get_context(true);
+        testing_env!(context);
+        assert!(bsh.handle_fee_gathering(svc, fa).is_ok());
     }
 }
