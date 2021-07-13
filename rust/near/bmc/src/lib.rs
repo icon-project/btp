a116b572cf44063421e1596ac9ed51449dc2d0de
shreyas.ks <shreyas.ks@hugobyte.com>
2021-07-14 00:47:55 +0530

    Add relay

--- rust/near/bmc/src/lib.rs
+++ rust/near/bmc/src/lib.rs
@@ -1,9 +1,12 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::AccountId;
 use near_sdk::{env, near_bindgen};
+use near_sdk::collections::{UnorderedMap, UnorderedSet};
 mod link;
 mod permission;
 mod route;
+
+
 use btp_common::BTPAddress;
 use permission::{Owner, Owners};
 use route::{Route, Routes};
@@ -21,7 +24,7 @@ pub struct BTPMessageCenter {
     message: Message,
     links: Links,
     routes: Routes,
-    owners: Owners,
+    // owners: Owners,
 }
 
 #[derive(Default, BorshDeserialize, BorshSerialize)]
@@ -35,12 +38,12 @@ impl Default for BTPMessageCenter {
         let message: Message = Default::default();
         let links = Links::new();
         let routes = Routes::new();
-        let owners = Owners::new();
+        // let owners = Owners::new();
         Self {
             message,
             links,
             routes,
-            owners,
+            // owners,
         }
     }
 }
@@ -55,39 +58,92 @@ impl BTPMessageCenter {
         self.message.payload = message.as_bytes().to_vec()
     }
 
-    pub fn add_link(&mut self, link: &BTPAddress) {
-        assert!(
-            self.owners.is_owner(env::signer_account_id()),
-            "BMCRevertUnauthorized"
-        );
+    // pub fn add_link(&mut self, link: &BTPAddress) {
+    //     assert!(
+    //         self.owners.is_owner(env::signer_account_id()),
+    //         "BMCRevertUnauthorized"
+    //     );
 
-        self.links
-            .add_link(link)
-            .expect_err("BMCRevertUnauthorized");
+    //     self.links
+    //         .add_link(link)
+    //         .expect_err("BMCRevertUnauthorized");
+    // }
+    // pub fn add_routes(&mut self, dst: &BTPAddress, link: &BTPAddress) {
+    //     self.routes
+    //         .add_route(dst, link)
+    //         .expect_err("Not able to add route");
+    // }
+
+    // pub fn remove_route(&mut self, dst: &BTPAddress) {
+    //     assert!(
+    //         self.owners.is_owner(env::signer_account_id()),
+    //         "BMCRevertUnauthorized"
+    //     );
+    //     self.routes.remove_route(dst).expect_err("Failed to remove");
+    // }
+
+    // pub fn add_owner(&mut self, address: &AccountId) {
+    //     assert!(
+    //         self.owners.is_owner(env::signer_account_id()),
+    //         "BMCRevertUnauthorized"
+    //     );
+
+    //     self.owners
+    //         .add_owner(address)
+    //         .expect_err("failed to add owner");
+    // }
+
+    pub fn add_relays(&mut self, link: &BTPAddress, address: &Vec<AccountId>) -> Vec<String> {
+        if let Some(mut link) = self.links.0.get(&link.to_string().into_bytes()) {
+            
+           
+            for add in address.to_vec().iter_mut() {
+
+               
+                link.relays.add_relay(add.to_string());
+
+
+               
+            }
+
+            return  link.relays.0.to_vec();
+
+        } 
+
+        return  vec![];
+
+        
+        // if self.links.is_connected(&link) {
+        //     for i in address.iter() {
+        //         let link 
+        //     }
+        // }
     }
-    pub fn add_routes(&mut self, dst: &BTPAddress, link: &BTPAddress) {
-        self.routes
-            .add_route(dst, link)
-            .expect_err("Not able to add route");
-    }
 
-    pub fn remove_route(&mut self, dst: &BTPAddress) {
-        assert!(
-            self.owners.is_owner(env::signer_account_id()),
-            "BMCRevertUnauthorized"
-        );
-        self.routes.remove_route(dst).expect_err("Failed to remove");
-    }
+    
 
-    pub fn add_owner(&mut self, address: &AccountId) {
-        assert!(
-            self.owners.is_owner(env::signer_account_id()),
-            "BMCRevertUnauthorized"
-        );
+    pub fn get_relays(&self,link:&BTPAddress) -> Vec<String> {
 
-        self.owners
-            .add_owner(address)
-            .expect_err("failed to add owner");
+      
+
+        let link = self.links.0.get(key: &K)
+
+            // if !link.relays.0.is_empty(){
+            //    for v in link.relays.0.iter(){
+
+            //    return v;
+            //    }
+            // }
+        
+
+        return vec![];
+
+        
+
+           
+
+        
+        
     }
 }
 #[cfg(test)]
@@ -127,5 +183,38 @@ mod tests {
         let mut s = String::from("");
         contract.send_message("dddddd".to_string());
         assert_eq!("dddddd".to_string(), contract.get_message());
+    }
+    #[test]
+    fn relay_handling(){
+        let context = get_context(vec![], false);
+        testing_env!(context);
+        let mut contract = BTPMessageCenter {
+            ..Default::default()
+        };
+
+        let link = BTPAddress("btp://0x1.near/cx87ed9048b594b95199f326fc76e76a9d33dd665b".to_string());
+
+        match contract.links.add_link(&link){
+
+            Ok(res) => println!("{}",res),
+            Err(err) => println!("{}",err)
+        }
+        let address  = vec![String::from("xyz"),String::from("abc"),String::from("tuv")];
+       
+       let result=  contract.add_relays(&link, &address);
+
+       println!("{:#?}",result);
+    
+
+        
+      let r = contract.get_relays(&link);
+
+      println!("{:?}",r);
+
+     
+
+      
+         
+       
     }
 }
