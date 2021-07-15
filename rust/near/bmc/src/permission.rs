bf745bdbd9a86cffbb4633ef86a58ae4f4a45479
shreyas.ks <shreyas.ks@hugobyte.com>
2021-07-15 22:29:47 +0530

    Update Owner permissions

--- rust/near/bmc/src/permission.rs
+++ rust/near/bmc/src/permission.rs
@@ -1,55 +1,50 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::env::*;
+use near_sdk::log;
 use near_sdk::AccountId;
 use std::collections::HashMap;
 
 #[derive(BorshDeserialize, BorshSerialize)]
-pub struct Owners(pub HashMap<AccountId, bool>);
+pub struct Owners(HashMap<AccountId, bool>);
 
-pub trait Owner {
-    fn new() -> Self;
-    fn add_owner(&mut self, address: &AccountId) -> Result<bool, String>;
-    fn remove_owner(&mut self, address: &AccountId) -> Result<bool, String>;
-    fn is_owner(&self, address: AccountId) -> bool;
-}
-
-impl Owner for Owners {
-    fn new() -> Self {
-        assert!(
-            current_account_id() == signer_account_id(),
-            "Dont have ppermission to call this method"
-        );
+impl Owners {
+    pub fn new() -> Option<Self> {
+        if current_account_id() != signer_account_id() {
+            return None;
+        }
         let mut owners = HashMap::new();
         owners.insert(signer_account_id(), true);
 
-        Self(owners)
+        return Some(Self(owners));
     }
 
-    fn add_owner(&mut self, address: &AccountId) -> Result<bool, String> {
-        assert!(
-            self.0.contains_key(&signer_account_id()),
-            "Caller does not have ppermission to add another owner"
-        );
+    pub fn add(&mut self, address: &AccountId) -> Result<bool, String> {
+        if self.0.contains_key(&signer_account_id()) {
+            if !self.0.contains_key(address) {
+                self.0.insert(address.to_string(), true);
 
-        return self
-            .0
-            .insert(address.to_string(), true)
-            .ok_or("Added Owner already present".to_string());
+                return Ok(true);
+            }
+            log!("Added owner already present");
+            return Ok(false);
+        }
+
+        return Err("Caller does not have ppermission to add another owner".to_string());
     }
-    fn remove_owner(&mut self, address: &AccountId) -> Result<bool, String> {
-        assert!(
-            self.0.contains_key(&signer_account_id()),
-            "Caller does not have ppermission to add another owner"
-        );
-        assert!(self.0.len() > 1, "BMCRevertNotExistsPermission");
+    pub fn remove(&mut self, address: &AccountId) -> Result<bool, String> {
+        if self.0.contains_key(&signer_account_id()) {
+            if self.0.len() > 1 {
+                self.0.remove(&address.to_string());
 
-        return self
-            .0
-            .remove(&address.to_string())
-            .ok_or("Owner not present".to_string());
+                return Ok(true);
+            }
+            return Err("BMCRevertNotExistsOwner".to_string());
+        }
+
+        return Err("Caller does not have ppermission to add another owner".to_string());
     }
 
-    fn is_owner(&self, address: AccountId) -> bool {
+    pub fn is(&self, address: AccountId) -> bool {
         self.0.contains_key(&address)
     }
 }
