b8fda10c170b8f7fdb57fbe231f51a37ead550db
shreyas.ks <shreyas.ks@hugobyte.com>
2021-07-19 00:39:30 +0530

    Update requests , Add Unit test

--- rust/near/bmc/src/service.rs
+++ rust/near/bmc/src/service.rs
@@ -1,18 +1,18 @@
-use near_sdk::borsh::{ self, BorshDeserialize, BorshSerialize};
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::UnorderedSet;
 use std::collections::HashMap;
 
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct BSH {
-   pub services: Services,
-   pub requests: Requests,
+    pub services: Services,
+    pub requests: Requests,
 }
 
 impl BSH {
-    pub fn new() ->Self{
-        Self{
+    pub fn new() -> Self {
+        Self {
             services: Services::new(),
-            requests : Requests::new()
+            requests: Requests::new(),
         }
     }
 }
@@ -21,34 +21,41 @@ impl BSH {
 pub struct Services(HashMap<String, String>);
 
 #[derive(BorshDeserialize, BorshSerialize)]
-pub struct Requests(UnorderedSet<String>);
+pub struct Requests(HashMap<String, String>);
 
 impl Requests {
     pub fn new() -> Self {
-        let requests = UnorderedSet::new(b"request".to_vec());
+        let requests = HashMap::new();
         Self(requests)
     }
-    
+    pub fn add(&mut self, name: String, address: String) -> Result<bool, String> {
+        if !self.0.contains_key(&name) {
+            self.0.insert(name.clone(), address);
+            return Ok(true);
+        }
+        return Err("BMCRevertAlreadyexist".to_string());
+    }
+    pub fn remove(&mut self, name: String) -> Result<bool, String> {
+        if self.0.contains_key(&name) {
+            self.0.remove(&name);
+            return Ok(true);
+        }
+        return Err("BMCRevertnotExist".to_string());
+    }
 
-    pub fn get(&self) -> Result<Vec<String>, String> {
+    pub fn get(&self, name: String) -> Result<String, String> {
+        if let Some(service) = self.0.get(&name) {
+            return Ok(service.to_string());
+        }
+        return Err("BMCRevertnotExist".to_string());
+    }
+
+    pub fn to_vec(&self) -> Result<Vec<(String, String)>, String> {
         if !self.0.is_empty() {
-            return Ok(self.0.to_vec());
+            return Ok(self.0.clone().into_iter().collect());
         }
 
         return Err("No pending request".to_string());
-    }
-    pub fn add(&mut self, service: String) -> Result<bool, String> {
-            if !self.0.contains(&service) {
-                return Ok(self.0.insert(&service));
-            }
-            return Err("BMCRevertAlreadyexist".to_string());  
-    }
-
-    pub fn remove(&mut self, service: String) -> Result<bool, String> {
-        if !self.0.is_empty() && self.0.contains(&service) {
-                return Ok(self.0.remove(&service));
-            }
-        return Err("BMCRevertnotExist".to_string());
     }
 }
 
@@ -76,7 +83,7 @@ impl Services {
 
     pub fn get(&self, name: String) -> Result<String, String> {
         if let Some(service) = self.0.get(&name) {
-            return Ok(service.to_string())
+            return Ok(service.to_string());
         }
         return Err("BMCRevertnotExist".to_string());
     }
@@ -116,7 +123,97 @@ mod tests {
         }
     }
 
-    
+    #[test]
+    fn add_pendingrequest_success() {
+        let context = get_context(vec![], false);
+        testing_env!(context);
+        let name = String::from("addservice");
+        let address = String::from("addressservice");
+        let mut bsh = BSH::new();
 
+        let result = bsh.requests.add(name.to_string(), address.to_string()).unwrap();
+
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn add_pendingrequest_fail() {
+        let context = get_context(vec![], false);
+        testing_env!(context);
+        let name = String::from("addservice");
+        let address = String::from("addressservice");
+        let mut bsh = BSH::new();
+
+        bsh.requests.add(name.to_string(), address.to_string()).unwrap();
+        let result2 = bsh.requests.add(name.to_string(), address.to_string()).unwrap_err();
+
+        assert_eq!(result2, "BMCRevertAlreadyexist");
+    }
+
+    #[test]
+    fn remove_pendingrequest_success() {
+        let context = get_context(vec![], false);
+        testing_env!(context);
+        let name = String::from("addservice");
+        let address = String::from("addressservice");
+        let mut bsh = BSH::new();
+
+        bsh.requests.add(name.to_string(), address.to_string()).unwrap();
+        let result = bsh.requests.remove(name.to_string()).unwrap();
+
+        assert_eq!(result, true);
+    }
+    #[test]
+    fn remove_pendingrequest_fail() {
+        let context = get_context(vec![], false);
+        testing_env!(context);
+        let name = String::from("addservice");
+        let address = String::from("addressservice");
+        let mut bsh = BSH::new();
+
+        bsh.requests.add(name.to_string(), address.to_string()).unwrap();
+        bsh.requests.remove(name.to_string()).unwrap();
+        let result = bsh.requests.remove(name.to_string()).unwrap_err();
+
+        assert_eq!(result, "BMCRevertnotExist");
+    }
+
+    #[test]
+    fn get_pendingrequest_success() {
+        let context = get_context(vec![], false);
+        testing_env!(context);
+        let name = String::from("addservice");
+        let address = String::from("addressservice");
+        let name1 = String::from("addservice1");
+        let address1 = String::from("addressservice1");
+        let mut bsh = BSH::new();
+
+        bsh.requests.add(name.to_string(), address.to_string()).unwrap();
+        bsh.requests.add(name1.to_string(), address1.to_string()).unwrap();
+        
+        let result = bsh.requests.get(name.to_string()).unwrap();
+
+        assert_eq!(result, address.to_string());
+    }
+   
+    // #[test]
+    // fn get_pendingrequests_success() {
+    //     let context = get_context(vec![], false);
+    //     testing_env!(context);
+    //     let name = String::from("addservice");
+    //     let address = String::from("addressservice");
+    //     let name1 = String::from("addservice1");
+    //     let address1 = String::from("addressservice1");
+    //     let mut bsh = BSH::new();
+
+    //     bsh.requests.add(name.to_string(), address.to_string()).unwrap();
+    //     bsh.requests.add(name1.to_string(), address1.to_string()).unwrap();
+
+    //     let vect = vec![(name1.to_string(), address1.to_string()),(name.to_string(), address.to_string())];
+        
+    //     let result = bsh.requests.to_vec().unwrap();
+
+    //     assert_eq!(result, vect);
+    // }
     
 }
