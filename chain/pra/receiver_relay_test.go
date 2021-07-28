6d856d8b2265964fed8e57394f64382c6f511f4d
Truong Ma Phi <trmaphi@gmail.com>
2021-07-28 17:09:18 +0700

    test: skip manual test

--- chain/pra/receiver_relay_test.go
+++ chain/pra/receiver_relay_test.go
@@ -7,6 +7,6 @@ import (
 )
 
 func TestNewRelayReceiver(t *testing.T) {
-	// t.Skip("Manual testing only")
+	t.Skip("Manual testing only")
 	NewRelayReceiver("wss://wss-relay.testnet.moonbeam.network/", "http://goloop.linhnc.info/api/v3/icondao", "cx4fd0306a26d8b812cf9cc5aacb68e2dab18fa14d", log.New())
 }
