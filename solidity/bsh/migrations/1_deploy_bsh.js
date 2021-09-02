91d3232107b59f69da90bb058ebc6f64ab63f25c
Quang Tran <dawnmasu@gmail.com>
2021-09-02 19:11:36 +0700

    add fixed fee in bsh migration file

--- solidity/bsh/migrations/1_deploy_bsh.js
+++ solidity/bsh/migrations/1_deploy_bsh.js
@@ -4,7 +4,7 @@ const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 
 module.exports = async function (deployer, network) {
   if (network !== "development") {
-    await deployProxy(BSHCore, [process.env.BSH_COIN_URL, process.env.BSH_COIN_NAME, parseInt(process.env.BSH_COIN_FEE)], { deployer });
+    await deployProxy(BSHCore, [process.env.BSH_COIN_URL, process.env.BSH_COIN_NAME, parseInt(process.env.BSH_FIXED_FEE)], { deployer });
     await deployProxy(BSHPeriphery, [process.env.BMC_PERIPHERY_ADDRESS, BSHCore.address, process.env.BSH_SERVICE], { deployer });
     const bshCore = await BSHCore.deployed();
     await bshCore.updateBSHPeriphery(BSHPeriphery.address);
