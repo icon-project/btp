5775c320537692dfbb8af56768a1f132704bf81d
mohamedelshami <1651925+mohamedelshami@users.noreply.github.com>
2021-09-15 13:24:44 +0100

    * Adding some manual tests for the relayer (unit tests needs improvement)

--- cmd/test/btpsimple_test_main.go
+++ cmd/test/btpsimple_test_main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/icon-project/btp/cmd/btpsimple/module"
 	"github.com/icon-project/btp/cmd/btpsimple/module/bsc"
 	"github.com/icon-project/btp/cmd/btpsimple/module/icon"
+	"github.com/icon-project/btp/common/codec"
 	"github.com/icon-project/btp/common/log"
+	"github.com/icon-project/btp/common/wallet"
+	"io/ioutil"
+	"math/big"
+	"strings"
 )
 
-func main() {
+func test_SenderReceiver() {
 	var wallet bsc.Wallet
 	var src, dst module.BtpAddress
 
@@ -47,5 +56,68 @@ func main() {
 		}
 		fmt.Println(j, relay)
 	}
+
+}
+
+func test_wallet() {
+	keyjson, err := ioutil.ReadFile("/Users/mo/web3labs/icon/btp/devnet/docker/icon-bsc/work/bsc.bsc.ks.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	//fmt.Println(keyjson)
+	key, err := keystore.DecryptKey(keyjson, "Perlia0")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(key)
+
+	keys, err := wallet.DecryptEvmKeyStore(keyjson, []byte("Perlia0"))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(keys)
+}
+
+func main() {
+	//var header types.Header
+	fmt.Println(strings.ToLower("btp://0x97.bsc/0xAaFc8EeaEE8d9C8bD3262CCE3D73E56DeE3FB776"))
+	var bytes []byte
+
+	header := types.Header{
+		Number: big.NewInt(300),
+	}
+
+	bytes, err := codec.RLP.MarshalToBytes(header)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	fmt.Println(bytes)
+	fmt.Println(header.Number)
+
+	var tmp types.Header
+	_, err = codec.RLP.UnmarshalFromBytes(bytes, &tmp)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	fmt.Println(tmp.Number)
+}
+
+func main1() {
+	var block types.Header
+	var bytes []byte
+
+	block.Number = big.NewInt(600)
+	bytes, err := rlp.EncodeToBytes(&block)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	//fmt.Println(bytes)
+	fmt.Println(hexutil.Encode(bytes))
+	var tmp types.Header
+	err = rlp.DecodeBytes(bytes, &tmp)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	fmt.Println(tmp.Number)
 
 }
