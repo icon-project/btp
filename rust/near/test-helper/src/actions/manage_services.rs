0b46f746743f1e2ac178f805a3871a09f292e77c
shreyas.ks <shreyas.ks@hugobyte.com>
2021-09-21 23:55:15 +0530

    Add methods for Services

--- rust/near/test-helper/src/actions/manage_services.rs
+++ rust/near/test-helper/src/actions/manage_services.rs
@@ -1,25 +1,36 @@
 use crate::{invoke_call, invoke_view};
-use crate::types::{Bmc, Context, Contract};
+use crate::types::{Bmc,Bsh, Context, Contract};
 use duplicate::duplicate;
 
 #[duplicate(
     contract_type;
     [ Bmc ];
+    [ Bsh ];
 )]
 
 impl Contract<'_, contract_type> {
-    pub fn add_service(&self, context: Context) -> Context {
-        invoke_call!(self, context, "add_service", method_params);
+    pub fn request_service(&self, context: Context) -> Context {
+        invoke_call!(self, context, "request_service", method_params);
         context
     }
 
-    pub fn remove_service(&self, context: Context) -> Context {
-        invoke_call!(self, context, "remove_service", method_params);
+    pub fn approve_service(&self, context: Context) -> Context {
+        invoke_call!(self, context, "approve_service", method_params);
+        context
+    }
+    
+    pub fn remove_service(&self, context: Context) -> Context{
+        invoke_call!(self, context, "remove_service",method_params);
         context
     }
 
     pub fn get_services(&self, mut context: Context) -> Context {
         invoke_view!(self, context, "get_services");
+        context
+    }
+
+    pub fn get_requests(&self, mut context: Context) -> Context{
+        invoke_view!(self, context, "get_requests");
         context
     }
 
