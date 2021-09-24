fc74289da5ac402e4815047dc6839134ca68c52c
shreyas.ks <shreyas.ks@hugobyte.com>
2021-09-24 12:49:33 +0530

    Add Relay test secenario

---
+++ rust/near/tests/steps/relay_management.rs
@@ -0,0 +1,6 @@
+use super::{BMC_CONTRACT, BSH_CONTRACT,BMV_CONTRACT};
+use serde_json::{json,from_value};
+use test_helper::types::Context;
+use super::*;
+
+pub static 
---
+++ rust/near/tests/test_manage_relays.rs
@@ -0,0 +1,75 @@
+mod steps;
+
+#[cfg(test)]
+mod manage_verifers {
+    use super::*;
+    use kitten::*;
+    use steps::*;
+
+    mod bmc {
+        use super::*;
+        
+        #[runner::test(sandbox)]
+        async fn add_relay_as_authorized_success(){\
+            Kitten::given(NEW_CONTEXT)
+                .and(BMC_CONTRACT_IS_DEPLOYED)
+                .when(ADD_RELAY_INVOKED_BY_BMC_OWNER)
+                .then(RELAYS_ARE_QURIED_IN_BMC)
+                .and(ADDED_RELAYS_SHOULD_BE_IN_LIST)
+
+        }
+
+        #[runner::test(sandbox)]
+        async fn add_relay_as_unauthorized_fail(){
+            Kitten::given(NEW_CONTEXT)
+                .and(BMC_CONTRACT_IS_DEPLOYED)
+                .when(ADD_RELAY_INVOKED_BY_NON_BMC_OWNER)
+                .then(BMC_SHOULD_THROW_UNAUTHORIZED_ERROR)
+        }
+
+        #[runner::test(sandbox)]
+        async fn add_relay_to_non_existing_link_authorized_fail(){
+            Kitten::given(NEW_CONTEXT)
+                .and(BMC_CONTRACT_IS_DEPLOYED)
+                .when(ADD_RELAY_WITH_NON_EXISTING_LINK_INVOKED_BY_BMC_OWNER)
+                .then(BMC_SHOULD_THROW_NOTEXIST_ERROR)
+
+        }
+
+        #[runner::test(sandbox)]
+        async fn remove_relay_as_authorized_success(){
+            Kitten::given(NEW_CONTEXT)
+                .and(BMC_CONTRACT_IS_DEPLOYED)
+                .when(REMOVE_RELAY_INVOKED_BY_BMC_OWNER)
+                .then(RELAYS_ARE_QURIED_IN_BMC)
+                .and(DELETED_RELAY_SHOULD_NOT_BE_IN_LIST)
+        }
+        
+        #[runner::test(sandbox)]
+        async fn remove_relay_as_unauthorized_success(){
+            Kitten::given(NEW_CONTEXT)
+            .and(BMC_CONTRACT_IS_DEPLOYED)
+            .when(REMOVE_RELAY_INVOKED_BY_NON_BMC_OWNER)
+            .then(BMC_SHOULD_THROW_UNAUTHORIZED_ERROR)
+        }
+
+        #[runner::test(sandbox)]
+        async fn remove_non_existing_relay_authorized_success(){
+            Kitten::given(NEW_CONTEXT)
+            .and(BMC_CONTRACT_IS_DEPLOYED)
+            .when(REMOVE_NON_EXISTING_RELAY_INVOKED_BY_BMC_OWNER)
+            .then(BMC_SHOULD_THROW_NOTEXIST_ERROR)
+        }
+
+        #[runner::test(sandbox)]
+        async fn remove_realy_with_invalid_link_authorized_fail(){
+            async fn remove_non_existing_relay_authorized_success(){
+                Kitten::given(NEW_CONTEXT)
+                .and(BMC_CONTRACT_IS_DEPLOYED)
+                .when(REMOVE_RELAY_WITH_INVALID_LINK_INVOKED_BY_BMC_OWNER)
+                .then(BMC_SHOULD_THROW_NOTEXIST_ERROR)
+        }
+
+    }
+
+}
