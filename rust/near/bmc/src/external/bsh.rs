5c417e64d009e9386b21a62d398f138811c652b3
Muhammed Irfan <muhammedirfan@hugobyte.com>
2021-10-18 05:13:00 +0530

    Bsh Cross contract api update

--- rust/near/bmc/src/external/bsh.rs
+++ rust/near/bmc/src/external/bsh.rs
@@ -1,7 +1,15 @@
-use near_sdk::{ ext_contract };
-use crate::BTPAddress;
+use crate::{BTPAddress, BtpMessage, SerializedMessage};
+use near_sdk::ext_contract;
 
 #[ext_contract(bsh_contract)]
 pub trait BshContract {
-    fn gather_fees(fee_aggregator: BTPAddress, service: String);
+    fn handle_btp_message(message: BtpMessage<SerializedMessage>);
+    fn handle_btp_error(
+        source: BTPAddress,
+        service: String,
+        serial_no: i128,
+        code: u128,
+        message: String,
+    );
+    fn handle_fee_gathering(fee_aggregator: BTPAddress, service: String);
 }
--- rust/near/bsh/nativecoin/src/external/mod.rs
+++ rust/near/bsh/nativecoin/src/external/mod.rs
@@ -2,3 +2,5 @@ mod native_coin;
 pub use native_coin::*;
 mod receiver;
 pub use receiver::*;
+mod bmc;
+pub use bmc::*;
