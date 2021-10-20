05b2b971fa86299cdf14c1856acd37780469beb6
mohamedelshami <1651925+mohamedelshami@users.noreply.github.com>
2021-10-20 21:06:06 +0100

    * Updating bsc node docker build to use recent bsc August PR update (v1.1.0-beta)

--- devnet/docker/bsc-node/build.sh
+++ devnet/docker/bsc-node/build.sh
@@ -1,19 +1,17 @@
 docker build -t bsc-build -<<EOF
 # Build BSC in an aphine builder container
-FROM golang:1.16-alpine3.13 as builder
+FROM golang:1.16-alpine3.13
 
-RUN apk add --no-cache gcc make git
+RUN apk add --no-cache make gcc musl-dev linux-headers git
 
-WORKDIR /home
-RUN git clone https://github.com/simsonraj/bsc
-WORKDIR /home/bsc
+# Checkout latest version on Feb 8th 2021
+RUN cd / && git clone https://github.com/web3labs/bsc \
+    && cd ./bsc && git checkout master && make geth
 
 # Update btcd version entry to fix checksum issue
-RUN sed -i 's/btcd v0.20.0-beta/btcd v0.21.0-beta/' go.mod
-RUN go mod download github.com/btcsuite/btcd
-RUN go mod download golang.org/x/crypto
-
-RUN make all
+#RUN sed -i 's/btcd v0.20.0-beta/btcd v0.21.0-beta/' go.mod
+#RUN go mod download github.com/btcsuite/btcd
+#RUN go mod download golang.org/x/crypto
 EOF
 
 #docker cp <containerID>:/home/bsc/build/bin/ .
