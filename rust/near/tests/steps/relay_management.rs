4f2855da5ea80e33dce8d21ce1bbc9784b3fd0df
Shreyas K S <shreyas.ks@hugobyte.com>
2022-02-09 03:47:01 +0530

    Update Relay testcases

--- rust/near/tests/steps/relay_management.rs
+++ rust/near/tests/steps/relay_management.rs
@@ -21,12 +21,13 @@ pub static BMC_OWNER_INVOKES_ADD_RELAY_IN_BMC: fn(Context) -> Context = |context
         .pipe(USER_INVOKES_ADD_RELAY_IN_BMC)
 };
 
-pub static RELAY_1_IS_REGISTERED: fn(Context) -> Context = |context: Context| -> Context {
-    context
-        .pipe(RELAY_1_ACCOUNT_IS_CREATED)
-        .pipe(ICON_LINK_ADDRESS_AND_RELAY_1_ARE_PROVIDED_AS_ADD_RELAY_PARAM)
-        .pipe(BMC_OWNER_INVOKES_ADD_RELAY_IN_BMC)
-};
+pub static RELAY_1_IS_REGISTERED_FOR_ICON_LINK: fn(Context) -> Context =
+    |context: Context| -> Context {
+        context
+            .pipe(RELAY_1_ACCOUNT_IS_CREATED)
+            .pipe(ICON_LINK_ADDRESS_AND_RELAY_1_ARE_PROVIDED_AS_ADD_RELAY_PARAM)
+            .pipe(BMC_OWNER_INVOKES_ADD_RELAY_IN_BMC)
+    };
 
 pub static ICON_LINK_ADDRESS_AND_RELAY_1_ACCOUNT_ID_AND_RELAY_2_ACCOUNT_ID_ARE_PROVIDED_AS_ADD_RELAYS_PARAM: fn(Context) -> Context =
     |mut context: Context| {
@@ -60,9 +61,12 @@ pub static ADDED_RELAYS_SHOULD_BE_IN_THE_LIST_OF_RELAYS: fn(Context) = |context:
         .unwrap()
         .into_iter()
         .collect();
-    let expected: HashSet<_> = vec![context.accounts().get("relay_1").id().to_string()]
-        .into_iter()
-        .collect();
+    let expected: HashSet<_> = vec![
+        context.accounts().get("relay_1").id().to_string(),
+        context.accounts().get("relay_2").id().to_string(),
+    ]
+    .into_iter()
+    .collect();
 
     assert_eq!(result, expected);
 };
@@ -77,4 +81,216 @@ pub static ICON_LINK_ADDRESS_IS_PROVIDED_AS_GET_RELAYS_PARAM: fn(Context) -> Con
         );
 
         context
+    };
+
+pub static ICON_LINK_ADDRESS_AND_RELAY_1_ACCOUNT_ID_ARE_PROVIDED_AS_REMOVE_RELAY_PARAM:
+    fn(Context) -> Context = |mut context: Context| {
+    context.add_method_params(
+        "remove_relay",
+        json!({
+            "link": format!("btp://{}/{}", ICON_NETWORK, ICON_BMC),
+            "relay": context.accounts().get("relay_1").id()
+        }),
+    );
+    context
+};
+
+pub static CHUCK_INVOKES_REMOVE_RELAY_IN_BMC: fn(Context) -> Context = |context: Context| {
+    context
+        .pipe(THE_TRANSACTION_IS_SIGNED_BY_CHUCK)
+        .pipe(USER_INVOKES_REMOVE_RELAY_IN_BMC)
+};
+
+pub static BMC_SHOULD_THROW_UNAUTHERISD_ERROR_ON_REMOVING_RELAY: fn(Context) =
+    |context: Context| {
+        let error = context.method_errors("remove_relay");
+        assert!(error.to_string().contains("BMCRevertNotExistsPermission"));
+    };
+
+pub static BSC_LINK_ADDRESS_AND_RELAY_1_ACCOUNT_ID_ARE_PROVIDED_AS_ADD_RELAYS_PARAM: fn(
+    Context,
+)
+    -> Context = |mut context: Context| {
+    context.add_method_params(
+        "add_relays",
+        json!({
+            "link": format!("btp://{}/{}", BSC_NETWORK, BSC_BMC),
+            "relays": [
+                context.accounts().get("relay_1").id()
+            ]
+        }),
+    );
+    context
+};
+
+pub static ALICE_INVOKES_ADD_RELAYS_IN_BMC: fn(Context) -> Context = |context: Context| {
+    context
+        .pipe(THE_TRANSACTION_IS_SIGNED_BY_ALICE)
+        .pipe(USER_INVOKES_ADD_RELAYS_IN_BMC)
+};
+
+pub static BMC_SHOULD_THROW_LINK_DOES_NOT_EXIST_ERROR_ON_ADDING_RELAYS: fn(Context) =
+    |context: Context| {
+        let error = context.method_errors("add_relays");
+        assert!(error.to_string().contains("BMCRevertNotExistsLink"));
+    };
+
+pub static ALICE_INVOKES_GET_RELAY_IN_BMC: fn(Context) -> Context = |context: Context| {
+    context
+        .pipe(THE_TRANSACTION_IS_SIGNED_BY_ALICE)
+        .pipe(USER_INVOKES_GET_RELAYS_IN_BMC)
+};
+
+pub static LIST_OF_RELAYS_SHOULD_EXIST_FOR_THE_GIVEN_LINK: fn(Context) = |context: Context| {
+    let relays = context.method_responses("get_relays");
+    let result: HashSet<_> = from_value::<Vec<String>>(relays)
+        .unwrap()
+        .into_iter()
+        .collect();
+    let expected: HashSet<_> = vec![context.accounts().get("relay_1").id().to_string()]
+        .into_iter()
+        .collect();
+
+    assert_eq!(result, expected);
+};
+
+pub static ICON_LINK_ADDRESS_AND_RELAY_1_ACCOUNT_ID_ARE_PROVIDED_AS_ADD_RELAYS_PARAM:
+    fn(Context) -> Context = |mut context: Context| {
+    context.add_method_params(
+        "add_relays",
+        json!({
+            "link": format!("btp://{}/{}", ICON_NETWORK, ICON_BMC),
+            "relays": [
+                context.accounts().get("relay_1").id()
+            ]
+        }),
+    );
+
+    context
+};
+
+pub static CHUCK_INVOKES_ADD_RELAYS_IN_BMC: fn(Context) -> Context = |context: Context| {
+    context
+        .pipe(THE_TRANSACTION_IS_SIGNED_BY_CHUCK)
+        .pipe(USER_INVOKES_ADD_RELAYS_IN_BMC)
+};
+
+pub static BMC_SHOULD_THROW_UNAUTHORIZED_ERROR_ON_ADDING_RELAYS: fn(Context) =
+    |context: Context| {
+        let error = context.method_errors("add_relays");
+        assert!(error.to_string().contains("BMCRevertNotExistsPermission"));
+    };
+
+pub static ALICE_INVOKES_REMOVE_RELAY_IN_BMC: fn(Context) -> Context = |context: Context| {
+    context
+        .pipe(THE_TRANSACTION_IS_SIGNED_BY_ALICE)
+        .pipe(USER_INVOKES_REMOVE_RELAY_IN_BMC)
+};
+
+pub static BMC_SHOULD_THROW_RELAY_DOES_NOT_EXIST_ON_REMOVING_NON_EXISTING_RELAY: fn(Context) =
+    |context: Context| {
+        let error = context.method_errors("remove_relay");
+        assert!(error.to_string().contains("BMCRevertNotExistRelay"));
+    };
+
+pub static BMC_SHOULD_THROW_UNAUTHORIZED_ERROR_ON_ADDING_RELAY: fn(Context) = |context: Context| {
+    let error = context.method_errors("add_relay");
+    assert!(error.to_string().contains("BMCRevertNotExistsPermission"));
+};
+
+pub static BMC_SHOULD_THROW_LINK_DOES_NOT_EXIST_ERROR_ON_ADDING_RELAY: fn(Context) =
+    |context: Context| {
+        let error = context.method_errors("add_relay");
+        assert!(error.to_string().contains("BMCRevertNotExistsLink"));
+    };
+
+pub static ICON_LINK_ADDRESS_AND_RELAY_1_ARE_PROVIDED_AS_REMOVE_RELAY_PARAM: fn(
+    Context,
+) -> Context = |mut context: Context| {
+    context.add_method_params(
+        "remove_relay",
+        json!({
+            "link": format!("btp://{}/{}", ICON_NETWORK, ICON_BMC),
+            "relay": context.accounts().get("relay_1").id()
+        }),
+    );
+
+    context
+};
+
+pub static CHUCK_INVOKES_ADD_RELAY_IN_BMC: fn(Context) -> Context = |context: Context| {
+    context
+        .pipe(THE_TRANSACTION_IS_SIGNED_BY_CHUCK)
+        .pipe(USER_INVOKES_ADD_RELAY_IN_BMC)
+};
+
+pub static REMOVED_RELAYS_SHOULD_NOT_BE_IN_THE_LIST_OF_RELAYS: fn(Context) = |context: Context| {
+    let context = context
+        .pipe(ICON_LINK_ADDRESS_IS_PROVIDED_AS_GET_RELAYS_PARAM)
+        .pipe(USER_INVOKES_GET_RELAYS_IN_BMC);
+
+    let relays = context.method_responses("get_relays");
+    let result: HashSet<_> = from_value::<Vec<String>>(relays).into_iter().collect();
+    let expected: HashSet<_> = from_value::<Vec<String>>(json!([])).into_iter().collect();
+
+    assert_eq!(result, expected);
+};
+
+pub static ADDED_RELAY_SHOULD_BE_IN_THE_LIST_OF_RELAYS: fn(Context) = |context: Context| {
+    let context = context
+        .pipe(ICON_LINK_ADDRESS_IS_PROVIDED_AS_GET_RELAYS_PARAM)
+        .pipe(USER_INVOKES_GET_RELAYS_IN_BMC);
+
+    let relays = context.method_responses("get_relays");
+    let result: HashSet<_> = from_value::<Vec<String>>(relays)
+        .unwrap()
+        .into_iter()
+        .collect();
+    let expected: HashSet<_> = vec![context.accounts().get("relay_1").id().to_string()]
+        .into_iter()
+        .collect();
+
+    assert_eq!(result, expected);
+};
+
+pub static NON_EXISTING_LINK_ADDRESS_PROVIDED_AS_GET_RELAY_PARAM: fn(Context) -> Context =
+    |mut context: Context| {
+        context.add_method_params(
+            "get_relays",
+            json!({
+                "link": format!("btp://{}/{}", BSC_NETWORK, BSC_BMC),
+            }),
+        );
+        context
+    };
+
+pub static USER_INVOKES_GET_RELAY_METHOD_WITH_NON_EXISTING_LINK: fn(Context) -> Context =
+    |context: Context| context.pipe(USER_INVOKES_GET_RELAYS_IN_BMC);
+
+pub static BMC_SHOULD_THROW_LINK_NOT_EXIST_ERROR_ON_GETTING_RELAYS: fn(Context) =
+    |context: Context| {
+        let error = context.method_errors("get_relays");
+        // assert!(error.to_string().contains("BMCRevertNotExistsRelay"));
+        println!("{}", error);
+    };
+
+pub static NON_EXISTING_LINK_ADDRESS_AND_RELAY1_IS_PROVIDED_AS_REMOVE_RELAY_PARAM_IN_BMC:
+    fn(Context) -> Context = |mut context: Context| {
+    context.add_method_params(
+        "remove_relay",
+        json!({
+            "link": format!("btp://{}/{}", BSC_NETWORK, BSC_BMC),
+            "relay": context.accounts().get("relay_1").id(),
+        }),
+    );
+    context
+};
+
+pub static BMC_OWNER_INVOKES_REMOVE_RELAY_IN_BMC: fn(Context) -> Context =
+    |context: Context| context.pipe(USER_INVOKES_REMOVE_RELAY_IN_BMC);
+
+pub static BMC_SHOULD_THROW_LINK_NOT_EXIST_ERROR_ON_REMOVE_RELAYS: fn(Context) =
+    |context: Context| {
+        let error = context.method_errors("remove_relay");
+        assert!(error.to_string().contains("BMCRevertNotExistsLink"));
     };
--- rust/near/tests/test_manage_relays.rs
+++ rust/near/tests/test_manage_relays.rs
@@ -1,7 +1,7 @@
 mod steps;
 
 #[cfg(test)]
-mod manage_verifers {
+mod manage_relays {
     use super::*;
     use kitten::*;
     use steps::*;
@@ -16,11 +16,167 @@ mod manage_verifers {
             .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
             .and(BMC_CONTRACT_IS_OWNED_BY_ALICE)
             .and(ICON_LINK_IS_PRESENT_IN_BMC)
-            .and(RELAY_1_IS_REGISTERED)
+            .and(RELAY_1_IS_REGISTERED_FOR_ICON_LINK)
             .and(RELAY_2_ACCOUNT_IS_CREATED)
             .and(ICON_LINK_ADDRESS_AND_RELAY_1_ACCOUNT_ID_AND_RELAY_2_ACCOUNT_ID_ARE_PROVIDED_AS_ADD_RELAYS_PARAM)
-            .when(ALICE_INVOKES_ADD_RELAY_IN_BMC)
+            .when(ALICE_INVOKES_ADD_RELAYS_IN_BMC)
             .then(ADDED_RELAYS_SHOULD_BE_IN_THE_LIST_OF_RELAYS);
         }
+
+        #[tokio::test(flavor = "multi_thread")]
+        async fn non_bmc_owner_cannot_delete_a_registered_bmr() {
+            Kitten::given(NEW_CONTEXT)
+                .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(CHUCKS_ACCOUNT_IS_CREATED)
+                .and(ICON_LINK_IS_PRESENT_IN_BMC)
+                .and(RELAY_1_IS_REGISTERED_FOR_ICON_LINK)
+                .and(ICON_LINK_ADDRESS_AND_RELAY_1_ACCOUNT_ID_ARE_PROVIDED_AS_REMOVE_RELAY_PARAM)
+                .when(CHUCK_INVOKES_REMOVE_RELAY_IN_BMC)
+                .then(BMC_SHOULD_THROW_UNAUTHERISD_ERROR_ON_REMOVING_RELAY);
+        }
+
+        #[tokio::test(flavor = "multi_thread")]
+        async fn bmc_owner_cannot_register_bmr_as_list_for_non_existing_link() {
+            Kitten::given(NEW_CONTEXT)
+                .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMC_CONTRACT_IS_OWNED_BY_ALICE)
+                .and(ICON_LINK_IS_PRESENT_IN_BMC)
+                .and(RELAY_1_ACCOUNT_IS_CREATED)
+                .and(BSC_LINK_ADDRESS_AND_RELAY_1_ACCOUNT_ID_ARE_PROVIDED_AS_ADD_RELAYS_PARAM)
+                .when(ALICE_INVOKES_ADD_RELAYS_IN_BMC)
+                .then(BMC_SHOULD_THROW_LINK_DOES_NOT_EXIST_ERROR_ON_ADDING_RELAYS);
+        }
+
+        #[tokio::test(flavor = "multi_thread")]
+        async fn non_bmc_owner_cannot_add_bmr_as_list() {
+            Kitten::given(NEW_CONTEXT)
+                .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(CHUCKS_ACCOUNT_IS_CREATED)
+                .and(VERIFIER_FOR_ICON_IS_ADDED)
+                .and(ICON_LINK_IS_PRESENT_IN_BMC)
+                .and(RELAY_1_ACCOUNT_IS_CREATED)
+                .and(ICON_LINK_ADDRESS_AND_RELAY_1_ACCOUNT_ID_ARE_PROVIDED_AS_ADD_RELAYS_PARAM)
+                .when(CHUCK_INVOKES_ADD_RELAYS_IN_BMC)
+                .then(BMC_SHOULD_THROW_UNAUTHORIZED_ERROR_ON_ADDING_RELAYS);
+        }
+
+        #[tokio::test(flavor = "multi_thread")]
+        async fn users_can_get_list_of_registered_relays_for_the_given_link() {
+            Kitten::given(NEW_CONTEXT)
+                .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMC_CONTRACT_IS_OWNED_BY_ALICE)
+                .and(VERIFIER_FOR_ICON_IS_ADDED)
+                .and(ICON_LINK_IS_PRESENT_IN_BMC)
+                .and(RELAY_1_IS_REGISTERED_FOR_ICON_LINK)
+                .and(ICON_LINK_ADDRESS_IS_PROVIDED_AS_GET_RELAYS_PARAM)
+                .when(ALICE_INVOKES_GET_RELAY_IN_BMC)
+                .then(LIST_OF_RELAYS_SHOULD_EXIST_FOR_THE_GIVEN_LINK);
+        }
+
+        #[tokio::test(flavor = "multi_thread")]
+        async fn bmc_owner_cannot_delete_a_non_existing_bmr() {
+            Kitten::given(NEW_CONTEXT)
+                .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMC_CONTRACT_IS_OWNED_BY_ALICE)
+                .and(ICON_LINK_IS_PRESENT_IN_BMC)
+                .and(RELAY_1_ACCOUNT_IS_CREATED)
+                .and(ICON_LINK_ADDRESS_AND_RELAY_1_ACCOUNT_ID_ARE_PROVIDED_AS_REMOVE_RELAY_PARAM)
+                .when(ALICE_INVOKES_REMOVE_RELAY_IN_BMC)
+                .then(BMC_SHOULD_THROW_RELAY_DOES_NOT_EXIST_ON_REMOVING_NON_EXISTING_RELAY);
+        }
+
+        #[tokio::test(flavor = "multi_thread")]
+        async fn bmc_owner_can_add_bmr_as_list() {
+            Kitten::given(NEW_CONTEXT)
+                .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMC_CONTRACT_IS_OWNED_BY_ALICE)
+                .and(ICON_LINK_IS_PRESENT_IN_BMC)
+                .and(RELAY_1_ACCOUNT_IS_CREATED)
+                .and(RELAY_2_ACCOUNT_IS_CREATED)
+                .and(ICON_LINK_ADDRESS_AND_RELAY_1_ACCOUNT_ID_AND_RELAY_2_ACCOUNT_ID_ARE_PROVIDED_AS_ADD_RELAYS_PARAM)
+                .when(ALICE_INVOKES_ADD_RELAYS_IN_BMC)
+                .then(ADDED_RELAYS_SHOULD_BE_IN_THE_LIST_OF_RELAYS);
+        }
+
+        #[tokio::test(flavor = "multi_thread")]
+        async fn non_bmc_owner_cannot_add_a_bmr() {
+            Kitten::given(NEW_CONTEXT)
+                .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(CHUCKS_ACCOUNT_IS_CREATED)
+                .and(BMC_CONTRACT_IS_NOT_OWNED_BY_CHUCK)
+                .and(ICON_LINK_IS_PRESENT_IN_BMC)
+                .and(RELAY_1_ACCOUNT_IS_CREATED)
+                .and(ICON_LINK_ADDRESS_AND_RELAY_1_ARE_PROVIDED_AS_ADD_RELAY_PARAM)
+                .when(CHUCK_INVOKES_ADD_RELAY_IN_BMC)
+                .then(BMC_SHOULD_THROW_UNAUTHORIZED_ERROR_ON_ADDING_RELAY);
+        }
+        #[tokio::test(flavor = "multi_thread")]
+        async fn bmc_owner_cannot_register_bmr_for_non_existing_link() {
+            Kitten::given(NEW_CONTEXT)
+                .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMC_CONTRACT_IS_OWNED_BY_ALICE)
+                .and(RELAY_1_IS_REGISTERED_FOR_ICON_LINK)
+                .and(ICON_LINK_ADDRESS_AND_RELAY_1_ARE_PROVIDED_AS_ADD_RELAY_PARAM)
+                .when(ALICE_INVOKES_ADD_RELAY_IN_BMC)
+                .then(BMC_SHOULD_THROW_LINK_DOES_NOT_EXIST_ERROR_ON_ADDING_RELAY);
+        }
+
+        #[tokio::test(flavor = "multi_thread")]
+        async fn bmc_owner_can_remove_a_registered_bmr() {
+            Kitten::given(NEW_CONTEXT)
+                .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMC_CONTRACT_IS_OWNED_BY_ALICE)
+                .and(ICON_LINK_IS_PRESENT_IN_BMC)
+                .and(RELAY_1_IS_REGISTERED_FOR_ICON_LINK)
+                .and(ICON_LINK_ADDRESS_AND_RELAY_1_ARE_PROVIDED_AS_REMOVE_RELAY_PARAM)
+                .when(ALICE_INVOKES_REMOVE_RELAY_IN_BMC)
+                .then(REMOVED_RELAYS_SHOULD_NOT_BE_IN_THE_LIST_OF_RELAYS);
+        }
+
+        #[tokio::test(flavor = "multi_thread")]
+        async fn bmc_owner_can_register_a_bmr() {
+            Kitten::given(NEW_CONTEXT)
+                .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMC_CONTRACT_IS_OWNED_BY_ALICE)
+                .and(ICON_LINK_IS_PRESENT_IN_BMC)
+                .and(RELAY_1_ACCOUNT_IS_CREATED)
+                .and(ICON_LINK_ADDRESS_AND_RELAY_1_ARE_PROVIDED_AS_ADD_RELAY_PARAM)
+                .when(ALICE_INVOKES_ADD_RELAY_IN_BMC)
+                .then(ADDED_RELAY_SHOULD_BE_IN_THE_LIST_OF_RELAYS);
+        }
+
+        #[tokio::test(flavor = "multi_thread")]
+        async fn users_cannot_get_list_of_relays_if_link_does_not_exist() {
+            Kitten::given(NEW_CONTEXT)
+              .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+              .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+              .and(ICON_LINK_IS_PRESENT_IN_BMC)
+              .and(RELAY_1_IS_REGISTERED_FOR_ICON_LINK)
+              .and(NON_EXISTING_LINK_ADDRESS_PROVIDED_AS_GET_RELAY_PARAM)
+              .when(USER_INVOKES_GET_RELAY_METHOD_WITH_NON_EXISTING_LINK)
+              .then(BMC_SHOULD_THROW_LINK_NOT_EXIST_ERROR_ON_GETTING_RELAYS);
+          }
+
+          #[tokio::test(flavor = "multi_thread")]
+          async fn bmc_owner_cannot_delete_bmr_for_non_existing_link(){
+            Kitten::given(NEW_CONTEXT)
+            .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+            .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+            .and(ICON_LINK_IS_PRESENT_IN_BMC)
+            .and(RELAY_1_IS_REGISTERED_FOR_ICON_LINK)
+            .and(NON_EXISTING_LINK_ADDRESS_AND_RELAY1_IS_PROVIDED_AS_REMOVE_RELAY_PARAM_IN_BMC)
+            .when(BMC_OWNER_INVOKES_REMOVE_RELAY_IN_BMC)
+            .then(BMC_SHOULD_THROW_LINK_NOT_EXIST_ERROR_ON_REMOVE_RELAYS);
+          }
     }
 }
