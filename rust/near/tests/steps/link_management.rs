aa29ed2a83ad742159f01ef5900121fd63764a51
shanithkk <kkshanith@gmail.com>
2022-02-09 04:01:37 +0530

    added the link testss

--- rust/near/tests/steps/link_management.rs
+++ rust/near/tests/steps/link_management.rs
@@ -118,3 +118,118 @@ pub static ICON_LINK_STATUS_SHOULD_BE_UPDATED: fn(Context) = |context: Context| 
     let result: LinkStatus = from_value(context.method_responses("get_status")).unwrap();
     assert_eq!(result.delay_limit(), 4);
 };
+
+pub static BMC_SHOULD_THROW_VERIFIER_NOT_EXISITING_ERROR: fn(Context) = |context: Context| {
+    let error = context.method_errors("add_link");
+    assert!(error.to_string().contains("BMCRevertNotExistBMV"));
+};
+
+pub static BMC_SHOULD_THROW_LINK_DOES_NOT_EXIST_ERROR_ON_SETTING_LINK: fn(Context) =
+    |context: Context| {
+        let error = context.method_errors("set_link");
+        assert!(error.to_string().contains("BMCRevertNotExistsLink"));
+    };
+
+pub static SET_LINK_IN_BMC_IS_INVOKED_BY_ALICE: fn(Context) -> Context = |context: Context| {
+        context
+            .pipe(THE_TRANSACTION_IS_SIGNED_BY_ALICE)
+            .pipe(USER_INVOKES_SET_LINK_IN_BMC)
+    };
+
+pub static ALICE_INVOKES_GET_STATUS_IN_BMC: fn(Context) -> Context =
+    |context: Context| context.pipe(USER_INVOKES_GET_STATUS_IN_BMC);
+
+pub static ICON_LINK_ADDRESS_IS_PROVIDED_AS_REMOVE_LINK_PARAM: fn(Context) -> Context =
+    |mut context: Context| {
+        context.add_method_params(
+            "remove_link",
+            json!({ "link": format!("btp://{}/{}", ICON_NETWORK, ICON_BMC) }),
+        );
+
+        context
+    };
+
+pub static ALICE_INVOKES_REMOVE_LINK_IN_BMC: fn(Context) -> Context = |context: Context| {
+        context
+            .pipe(THE_TRANSACTION_IS_SIGNED_BY_ALICE)
+            .pipe(USER_INVOKES_REMOVE_LINK_IN_BMC)
+    };
+
+pub static BMC_SHOULD_THROW_LINK_NOT_EXISTING_ERROR: fn(Context) = |context: Context| {
+        let error = context.method_errors("remove_link");
+        assert!(error.to_string().contains("BMCRevertNotExistsLink"));
+    };
+
+pub static SET_LINK_IN_BMC_IS_INVOKED_ALICE: fn(Context) -> Context = |context: Context| {
+        context
+            .pipe(THE_TRANSACTION_IS_SIGNED_BY_ALICE)
+            .pipe(ICON_LINK_ADDRESS_IS_PROVIDED_AS_ADD_LINK_PARAM)
+            .pipe(USER_INVOKES_SET_LINK_IN_BMC)
+    };
+
+pub static MAX_AGGREGATION_IS_PROVIDED_AS_SET_LINK_PARAM: fn(Context) -> Context =
+    |mut context: Context| {
+        context.add_method_params(
+            "set_link",
+            json!({
+                "max_aggregation": 10,
+            }),
+        );
+
+        context
+    };
+
+pub static MAX_AGGREGATION_IN_ICON_LINK_STATUS_SHOULD_BE_UPDATED: fn(Context) =
+    |context: Context| {
+        let context = context
+            .pipe(ICON_LINK_ADDRESS_IS_PROVIDED_AS_GET_STATUS_PARAM)
+            .pipe(USER_INVOKES_GET_STATUS_IN_BMC);
+        let result: LinkStatus = from_value(context.method_responses("get_status")).unwrap();
+        // TODO: complete test case
+    };
+
+pub static CHUCK_INVOKES_SET_LINK_IN_BMC: fn(Context) -> Context = |context: Context| {
+        context
+            .pipe(THE_TRANSACTION_IS_SIGNED_BY_CHUCK)
+            .pipe(USER_INVOKES_SET_LINK_IN_BMC)
+    };
+
+pub static BMC_SHOULD_THROW_UNAUTHORIZED_ERROR_ON_SETTING_LINK: fn(Context) = |context: Context| {
+        let error = context.method_errors("set_link");
+        assert!(error.to_string().contains("BMCRevertNotExistsPermission"));
+    };
+
+pub static ICON_LINK_ADDRESS_WITH_0_DELAY_IS_PROVIDED_AS_SET_LINK_PARAM: fn(Context) -> Context =
+    |mut context: Context| {
+        context.add_method_params(
+            "set_link",
+            json!({
+                "link": format!("btp://{}/{}", ICON_NETWORK, ICON_BMC),
+                "block_interval": 15000,
+                "max_aggregation": 5,
+                "delay_limit": 0
+            }),
+        );
+
+        context
+    };
+
+pub static BMC_SHOULD_THROW_INVALID_PARAM_ERROR_ON_SET_LINK: fn(Context) = |context: Context| {
+        let error = context.method_errors("set_link");
+        assert!(error.to_string().contains("BMCRevertInvalidParam"));
+    };
+
+pub static ICON_LINK_ADDRESS_WITH_0_MAX_AGGREGATION_IS_PROVIDED_AS_SET_LINK_PARAM: fn(
+        Context,
+    ) -> Context = |mut context: Context| {
+        context.add_method_params(
+            "set_link",
+            json!({
+                "link": format!("btp://{}/{}", ICON_NETWORK, ICON_BMC),
+                "block_interval": 15000,
+                "max_aggregation": 0,
+                "delay_limit": 4
+            }),
+        );
+        context
+    };
--- rust/near/tests/test_manage_links.rs
+++ rust/near/tests/test_manage_links.rs
@@ -40,10 +40,111 @@ mod manage_links {
             .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
             .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
             .and(BMC_CONTRACT_IS_OWNED_BY_ALICE)
-            .and(ICON_LINK_IS_ADDED)
+            .and(ICON_LINK_IS_PRESENT_IN_BMC)
             .and(ICON_LINK_ADDRESS_IS_PROVIDED_AS_SET_LINK_PARAM)
             .when(ALICE_INVOKES_SET_LINK_IN_BMC)
             .then(ICON_LINK_STATUS_SHOULD_BE_UPDATED);
+        }
+
+        #[tokio::test(flavor = "multi_thread")]
+        async fn bmc_owner_cannot_add_link_if_the_verifier_does_not_exist(){
+            Kitten::given(NEW_CONTEXT)
+            .and(BMC_CONTRACT_IS_DEPLOYED)
+            .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+            .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+            .and(BMC_CONTRACT_IS_OWNED_BY_ALICE)
+            .and(ICON_LINK_ADDRESS_IS_PROVIDED_AS_ADD_LINK_PARAM)
+            .when(ALICE_INVOKES_ADD_LINK_IN_BMC)
+            .then(BMC_SHOULD_THROW_VERIFIER_NOT_EXISITING_ERROR);
+    }
+
+        #[tokio::test(flavor = "multi_thread")]
+        async fn bmc_owner_cannot_set_link_config_if_the_link_is_not_registered() {
+            Kitten::given(NEW_CONTEXT)
+                .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMC_CONTRACT_IS_OWNED_BY_ALICE)
+                .and(ICON_LINK_ADDRESS_IS_PROVIDED_AS_SET_LINK_PARAM)
+                .when(ALICE_INVOKES_SET_LINK_IN_BMC)
+                .then(BMC_SHOULD_THROW_LINK_DOES_NOT_EXIST_ERROR_ON_SETTING_LINK);
+        }
+
+        #[tokio::test(flavor = "multi_thread")]
+        async fn bmr_operator_can_get_link_status_of_connected_bmc() {
+            Kitten::given(NEW_CONTEXT)
+                .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMC_CONTRACT_IS_OWNED_BY_ALICE)
+                .and(ICON_LINK_IS_PRESENT_IN_BMC)
+                .and(ICON_LINK_ADDRESS_IS_PROVIDED_AS_SET_LINK_PARAM)
+                .and(SET_LINK_IN_BMC_IS_INVOKED_BY_ALICE)
+                .and(ICON_LINK_ADDRESS_IS_PROVIDED_AS_GET_STATUS_PARAM)
+                .when(ALICE_INVOKES_GET_STATUS_IN_BMC)
+                .then(ICON_LINK_STATUS_SHOULD_BE_UPDATED);
+        }
+
+        #[tokio::test(flavor = "multi_thread")]
+        async fn bmc_owner_cannot_remove_a_link_if_link_does_not_exist() {
+            Kitten::given(NEW_CONTEXT)
+                .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMC_CONTRACT_IS_OWNED_BY_ALICE)
+                .and(VERIFIER_FOR_ICON_IS_ADDED)
+                .and(ICON_LINK_ADDRESS_IS_PROVIDED_AS_REMOVE_LINK_PARAM)
+                .when(ALICE_INVOKES_REMOVE_LINK_IN_BMC)
+                .then(BMC_SHOULD_THROW_LINK_NOT_EXISTING_ERROR);
+        }
+
+        #[tokio::test(flavor = "multi_thread")]
+        async fn set_max_aggregation_to_link_status() {
+            Kitten::given(NEW_CONTEXT)
+                .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+                .and(BMC_CONTRACT_IS_OWNED_BY_ALICE)
+                .and(ICON_LINK_IS_PRESENT_IN_BMC)
+                .and(ICON_LINK_ADDRESS_IS_PROVIDED_AS_SET_LINK_PARAM)
+                .and(SET_LINK_IN_BMC_IS_INVOKED_ALICE)
+                .and(MAX_AGGREGATION_IS_PROVIDED_AS_SET_LINK_PARAM)
+                .when(ALICE_INVOKES_SET_LINK_IN_BMC)
+                .then(MAX_AGGREGATION_IN_ICON_LINK_STATUS_SHOULD_BE_UPDATED);
+        }
+
+        #[tokio::test(flavor = "multi_thread")]
+        async fn set_link_as_unauthorized_success(){
+            Kitten::given(NEW_CONTEXT)
+            .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+            .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+            .and(BMC_CONTRACT_IS_OWNED_BY_ALICE)
+            .and(CHUCKS_ACCOUNT_IS_CREATED)
+            .and(BMC_CONTRACT_IS_NOT_OWNED_BY_CHUCK)
+            .and(ICON_LINK_IS_PRESENT_IN_BMC)
+            .and(ICON_LINK_ADDRESS_IS_PROVIDED_AS_SET_LINK_PARAM)
+            .when(CHUCK_INVOKES_SET_LINK_IN_BMC)
+            .then(BMC_SHOULD_THROW_UNAUTHORIZED_ERROR_ON_SETTING_LINK);
+        }
+
+        #[tokio::test(flavor = "multi_thread")]
+        async fn set_link_with_delay_limit_less_than_1_fails(){
+            Kitten::given(NEW_CONTEXT)
+            .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+            .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+            .and(BMC_CONTRACT_IS_OWNED_BY_ALICE)
+            .and(ICON_LINK_IS_PRESENT_IN_BMC)
+            .and(ICON_LINK_ADDRESS_WITH_0_DELAY_IS_PROVIDED_AS_SET_LINK_PARAM)
+            .when(ALICE_INVOKES_SET_LINK_IN_BMC)
+            .then(BMC_SHOULD_THROW_INVALID_PARAM_ERROR_ON_SET_LINK);
+        }
+
+        #[tokio::test(flavor = "multi_thread")]
+        async fn bmc_owner_cannot_set_link_config_with_max_aggregation_param_less_than_1(){
+            Kitten::given(NEW_CONTEXT)
+            .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+            .and(BMV_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
+            .and(BMC_CONTRACT_IS_OWNED_BY_ALICE)
+            .and(ICON_LINK_IS_PRESENT_IN_BMC)
+            .and(ICON_LINK_ADDRESS_WITH_0_MAX_AGGREGATION_IS_PROVIDED_AS_SET_LINK_PARAM)
+            .when(ALICE_INVOKES_SET_LINK_IN_BMC)
+            .then(BMC_SHOULD_THROW_INVALID_PARAM_ERROR_ON_SET_LINK);
         }
     }
 }
