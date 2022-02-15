a1657aa9c75a01fc7c8ea7a20a1b236768ecd245
Shreyas K S <shreyas.ks@hugobyte.com>
2022-02-15 13:05:48 +0530

    fix typos

--- rust/near/tests/test_manage_fees.rs
+++ rust/near/tests/test_manage_fees.rs
@@ -79,7 +79,7 @@ mod manage_bsh_services {
         }
 
         #[tokio::test(flavor = "multi_thread")]
-        async fn bsh_owner_can_register_new_wrapped_native_COIN() {
+        async fn bsh_owner_can_register_new_wrapped_native_coin() {
             Kitten::given(NEW_CONTEXT)
                 .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
                 .and(NATIVE_COIN_BSH_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
@@ -91,7 +91,7 @@ mod manage_bsh_services {
         }
 
         #[tokio::test(flavor = "multi_thread")]
-        async fn non_bsh_owner_cannot_register_new_wrapped_native_COIN() {
+        async fn non_bsh_owner_cannot_register_new_wrapped_native_coin() {
             Kitten::given(NEW_CONTEXT)
                 .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
                 .and(NATIVE_COIN_BSH_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
@@ -104,7 +104,7 @@ mod manage_bsh_services {
         }
 
         #[tokio::test(flavor = "multi_thread")]
-        async fn new_bsh_owner_can_register_new_wrapped_native_COin() {
+        async fn new_bsh_owner_can_register_new_wrapped_native_coin() {
             Kitten::given(NEW_CONTEXT)
                 .and(BMC_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
                 .and(NATIVE_COIN_BSH_CONTRACT_IS_DEPLOYED_AND_INITIALIZED)
@@ -114,7 +114,7 @@ mod manage_bsh_services {
                 .and(CHARLIES_ACCOUNT_ID_IS_PROVIDED_AS_ADD_OWNER_PARAM)
                 .and(BOB_INVOKES_ADD_OWNER_IN_NATIVE_COIN_BSH)
                 .and(NEW_COIN_NAME_IS_PROVIDED_AS_REGISTER_WARPPED_COIN_PARAM)
-                .when(CHARLIE_INVOKES_REGISTER_NEW_WRAPPED_COIN_IN_BSH)
+                .when(CHARLIE_INVOKES_REGISTER_NEW_WRAPPED_COIN_IN_NATIVE_COIN_BSH)
                 .then(COIN_REGISTERED_SHOULD_BE_PRESENT);
         }
     }
@@ -170,7 +170,5 @@ mod manage_bsh_services {
         .then(TOKEN_ID_SHOULD_BE_PRESENT_FOR_THE_REGISTERED_TOKEN);
 
        }
-
-
     }
 }
