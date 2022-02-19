7bca96a4fef867409663e85c37886f201f92feae
Shreyas K S <shreyas.ks@hugobyte.com>
2022-02-19 11:52:47 +0530

    Add Native coin factory

--- rust/near/bsh/nativecoin/src/coin_management.rs
+++ rust/near/bsh/nativecoin/src/coin_management.rs
@@ -10,23 +10,51 @@ impl NativeCoinService {
 
     /// Register Coin, Accept coin meta(name, symbol, network, denominator) as parameters
     // TODO: Complete Documentation
+    #[payable]
     pub fn register(&mut self, coin: Coin) {
         self.assert_have_permission();
         self.assert_coin_does_not_exists(&coin);
-
-        let coin_id = Self::hash_coin_id(coin.name());
-        // coin id is hash(coin_name)
-        self.coins.add(&coin_id, &coin);
-        self.coin_fees.add(&coin_id);
-        if coin.network() != &self.network {
-            self.registered_coins.add(
+        
+        if coin.network() == &self.network {
+            self.register_coin_callback(coin);
+        } else {
+            let coin_metadata = coin.extras().clone().expect("Coin Metadata Missing");
+            let promise_idx = env::promise_batch_create(
                 &coin.metadata().uri_deref().expect("Coin Account Missing"),
-                &coin_id,
             );
-        };
-
-        // Sets initial balance for self
-        self.balances.add(&env::current_account_id(), &coin_id);
+            env::promise_batch_action_create_account(promise_idx);
+            env::promise_batch_action_transfer(promise_idx, env::attached_deposit());
+            env::promise_batch_action_deploy_contract(promise_idx, NEP141_CONTRACT);
+            env::promise_batch_action_function_call(
+                promise_idx,
+                "new",
+                &json!({
+                    "owner_id": env::current_account_id(),
+                    "total_supply": U128(0),
+                    "metadata": {
+                        "spec": coin_metadata.spec.clone(),
+                        "name": coin.name(),
+                        "symbol": coin.symbol(),
+                        "icon": coin_metadata.icon.clone(),
+                        "reference": coin_metadata.reference.clone(),
+                        "reference_hash": coin_metadata.reference_hash.clone(),
+                        "decimals": coin_metadata.decimals.clone()
+                    }
+                })
+                .to_string()
+                .into_bytes(),
+                0,
+                estimate::GAS_FOR_RESOLVE_TRANSFER,
+            );
+            env::promise_then(
+                promise_idx,
+                env::current_account_id(),
+                "register_coin_callback",
+                &json!({ "coin": coin }).to_string().into_bytes(),
+                0,
+                estimate::GAS_FOR_RESOLVE_TRANSFER,
+            );
+        }
     }
 
     // TODO: Unregister Token
@@ -75,16 +103,25 @@ impl NativeCoinService {
 
         log!("[Burn] {} {}", amount, coin_symbol);
     }
+
+    #[private]
+    pub fn register_coin_callback(&mut self, coin: Coin) {
+        let coin_id = Self::hash_coin_id(coin.name());
+
+        self.coins.add(&coin_id, &coin);
+        self.coin_fees.add(&coin_id);
+
+        self.registered_coins.add(
+            &coin.metadata().uri_deref().expect("Coin Account Missing"),
+            &coin_id,
+        );
+
+        self.balances.add(&env::current_account_id(), &coin_id);
+    }
 }
 
 impl NativeCoinService {
-    pub fn mint(
-        &mut self,
-        coin_id: &CoinId,
-        amount: u128,
-        coin: &Coin,
-        receiver_id: AccountId,
-    ) {
+    pub fn mint(&mut self, coin_id: &CoinId, amount: u128, coin: &Coin, receiver_id: AccountId) {
         ext_nep141::mint(
             amount.into(),
             coin.metadata().uri().to_owned().unwrap(),
