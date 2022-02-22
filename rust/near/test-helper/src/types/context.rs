40737ecfc86533560b1ddd3606b0d6d952be00d5
Immanuel1520 <immanuel.stanley@hugobyte.com>
2022-02-22 14:28:50 +0530

    test worker updated

--- rust/near/test-helper/src/types/context.rs
+++ rust/near/test-helper/src/types/context.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 use workspaces::{Worker, Account as WorkspaceAccount, Sandbox, sandbox,Testnet,testnet};
 
 pub struct Context {
-    worker: Worker<Testnet>,
+    worker: Worker<Sandbox>,
     contracts: Contracts,
     accounts: Accounts,
     signer: Option<WorkspaceAccount>,
@@ -16,7 +16,7 @@ pub struct Context {
 impl Context {
     pub fn new() -> Context {
         Context {
-            worker: testnet(),
+            worker: sandbox(),
             contracts: Contracts::default(),
             accounts: Accounts::default(),
             signer: None,
@@ -26,7 +26,7 @@ impl Context {
         }
     }
 
-    pub fn worker(&self) -> &Worker<Testnet> {
+    pub fn worker(&self) -> &Worker<Sandbox> {
         &self.worker
     }
 
