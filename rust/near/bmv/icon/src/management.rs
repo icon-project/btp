fedaf3e7d56ddb0a44c4de7731a8210b0f069150
Muhammed Irfan <muhammedirfan@hugobyte.com>
2022-02-22 17:06:00 +0530

    fix bmv panic and verifier response

--- rust/near/bmv/icon/src/management.rs
+++ rust/near/bmv/icon/src/management.rs
@@ -12,21 +12,4 @@ impl BtpMessageVerifier {
     pub fn status(&self) -> VerifierStatus {
         VerifierStatus::new(self.mta.height(), self.mta.offset(), self.last_height)
     }
-
-    pub fn update_state(&mut self, state_changes: &mut StateChanges) {
-        state_changes.retain(|state_change| {
-            match state_change {
-                StateChange::SetValidators { validators } => {
-                    self.validators.set(validators);
-                }
-                StateChange::MtaAddBlockHash { block_hash } => {
-                    // self.mta.add(*block_hash);
-                }
-                StateChange::SetLastHeight { height} => {
-                    self.last_height.clone_from(height);
-                }
-            };
-            false
-        });
-    }
 }
