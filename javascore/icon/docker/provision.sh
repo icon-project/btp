29c20da7bcd9a028146e5e7bbc1882bf3c5b1e46
Muhammed Irfan <muhammedirfan@hugobyte.com>
2022-02-25 01:21:58 +0530

    update step limit

--- javascore/icon/docker/provision.sh
+++ javascore/icon/docker/provision.sh
@@ -8,6 +8,9 @@ provision() {
     ensure_server_start
     cd ./config
     goloop gn gen --out genesis.json $GOLOOP_KEY_STORE
+    
+    echo $(cat genesis.json | jq -r '.*{"chain":{"fee":{"stepLimit":{"invoke":"0x10000000","query":"0x1000000"}}}}') > genesis.json
+    
     goloop chain join --genesis_template genesis.json --channel icon --auto_start > chain_id
     goloop chain start icon
     server_stop
