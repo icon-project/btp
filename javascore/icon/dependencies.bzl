77b1cfbacc3ec005157d9e7afba45d9cff7d966a
Muhammed Irfan <muhammedirfan@hugobyte.com>
2022-02-25 17:09:47 +0530

    add goloop config dir

--- javascore/icon/dependencies.bzl
+++ javascore/icon/dependencies.bzl
@@ -5,7 +5,7 @@ def dependencies():
     container_pull(
         name = "goloop_base",
         registry = "index.docker.io",
-        repository = "iconloop/goloop",
-        tag = "latest",
+        repository = "iconloop/goloop-icon",
+        tag = "v0.9.9",
     )
     util_dependencies()
--- javascore/icon/docker/provision.sh
+++ javascore/icon/docker/provision.sh
@@ -9,7 +9,7 @@ provision() {
     cd ./config
     goloop gn gen --out genesis.json $GOLOOP_KEY_STORE
     
-    echo $(cat genesis.json | jq -r '.*{"chain":{"fee":{"stepLimit":{"invoke":"0x10000000","query":"0x1000000"}}}}') > genesis.json
+    echo $(cat genesis.json | jq -r '.*{"chain":{"fee":{"stepLimit":{"invoke":"0x12A05F200","query":"0x2faf080"}}}}') > genesis.json
     
     goloop chain join --genesis_template genesis.json --channel icon --auto_start > chain_id
     goloop chain start icon
