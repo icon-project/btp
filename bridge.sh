da836f776d76afd0c62e31ba7bf51f17663d2b5d
Muhammed Irfan <muhammedirfan@hugobyte.com>
2022-03-30 05:53:20 +0300

    add sleep for node_url

--- bridge.sh
+++ bridge.sh
@@ -15,6 +15,6 @@ fi
 
 bazel run "@${SOURCE}//:${SOURCE}_node_image"
 bazel run "@${DESTINATION}//:${DESTINATION}_node_image"
-bazel run @btp//cmd/btpsimple:btpsimple_image
+bazel run @btp//cmd/btpsimple:btpsimple_image --incompatible_enable_cc_toolchain_resolution --extra_toolchains @zig_sdk//:linux_amd64_gnu.2.19_toolchain
 
 bazel run "@btp//bridge:${SOURCE}_and_${DESTINATION}" --verbose_failures --define network=testnet
