9170d1030a201aaaa2df8cc42d9fd0f28e6fef3e
EunKi Yu <yueunki@iconloop.com>
2022-06-09 11:34:55 +0900

    Add tests for message proof

---
+++ solidity/bmv/contracts/mocks/MessageProofMock.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.4.22 < 0.9.0;
+
+import "../MessageProofLib.sol";
+
+contract MessageProofMock {
+
+    using MessageProofLib for MessageProofLib.MessageProof;
+
+    function decode(bytes memory enc) public returns (MessageProofLib.MessageProof memory) {
+        MessageProofLib.MessageProof memory mp = MessageProofLib.decode(enc);
+        return mp;
+    }
+
+    function calculate(bytes memory enc) public returns (bytes32) {
+        MessageProofLib.MessageProof memory mp = MessageProofLib.decode(enc);
+        return mp.calculate();
+    }
+
+}
+
