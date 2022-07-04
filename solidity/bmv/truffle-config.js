40e4c579a3703b2ad99da3c3f03cf09d1a5d7121
EunKi Yu <yueunki@iconloop.com>
2022-07-04 14:47:27 +0900

    Delete unused truffle configurations

--- solidity/bmv/truffle-config.js
+++ solidity/bmv/truffle-config.js
@@ -4,17 +4,6 @@ module.exports = {
             host: "127.0.0.1",
             port: 8545,
             network_id: "*",
-            args: {
-                bmc: "0xf02D03eC07768fc3a5C2e8C1Be734E529a49725C",
-                validators: {
-                    addresses: [
-                        "0xf02D03eC07768fc3a5C2e8C1Be734E529a49725C",
-                        "0x9515864E092308aB8F0634A6273264F86b50B8CE",
-                        "0x01179d8BB693BD851f53d65c30741A845fF0fa81",
-                        "0x99F276247abce7cfeF24Fbb2e3f0056A3Ca6CFE0"
-                    ]
-                }
-            }
         },
     },
 
