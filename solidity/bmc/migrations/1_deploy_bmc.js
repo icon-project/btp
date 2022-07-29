13cefc7ac9cffca0d2c90b7364d66298ae935013
EunKi Yu <yueunki@iconloop.com>
2022-07-29 14:34:11 +0900

    Update bmc migration description
     - deployment with development network
     - change environment variable name

--- solidity/bmc/migrations/1_deploy_bmc.js
+++ solidity/bmc/migrations/1_deploy_bmc.js
@@ -3,11 +3,9 @@ const BMCPeriphery = artifacts.require('BMCPeriphery');
 const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 
 module.exports = async function (deployer, network) {
-  if (network !== "development") {
     await deployProxy(BMCManagement, { deployer });
-    await deployProxy(BMCPeriphery, [process.env.BMC_PRA_NET, BMCManagement.address], { deployer });
+    await deployProxy(BMCPeriphery, [process.env.BMC_NETWORK_ID, BMCManagement.address], { deployer });
 
     const bmcManagement = await BMCManagement.deployed();
     await bmcManagement.setBMCPeriphery(BMCPeriphery.address);
-  }
 };
