51f6cdcbbc7f5d78182d7927c45a3226fff03307
EunKi Yu <yueunki@iconloop.com>
2022-08-02 14:18:42 +0900

    Support serialized extra status of bmv

--- solidity/bmv/contracts/interfaces/IBMV.sol
+++ solidity/bmv/contracts/interfaces/IBMV.sol
@@ -1,29 +1,36 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity > 0.5.8;
-pragma experimental ABIEncoderV2;
+pragma solidity >=0.8.0;
+pragma abicoder v2;
 
-interface IBtpMessageVerifier {
-
-    function getStatus() external view returns (uint, uint, uint, uint);
+interface IBMV {
+    /**
+        @notice Used by the relay to resolve next BTP Message to send.
+                Called by BMC.
+        @return height Last verified block height
+        @return extra  extra rlp encoded bytes
+     */
+    function getStatus()
+        external
+        view
+        returns (
+            uint256 height,
+            bytes memory extra
+        );
 
     /**
         @notice Decodes Relay Messages and process BTP Messages.
                 If there is an error, then it sends a BTP Message containing the Error Message.
-                BTP Messages with old sequence numbers are ignored. A BTP Message contains future
-                sequence number will fail.
+                BTP Messages with old sequence numbers are ignored. A BTP Message contains future sequence number will fail.
         @param _bmc BTP Address of the BMC handling the message
         @param _prev BTP Address of the previous BMC
         @param _seq next sequence number to get a message
         @param _msg serialized bytes of Relay Message
         @return serializedMessages List of serialized bytes of a BTP Message
-    */
+     */
     function handleRelayMessage(
         string memory _bmc,
         string memory _prev,
         uint256 _seq,
-        bytes memory _msg
-    )
-    external
-    returns (bytes[] memory);
-
+        bytes calldata _msg
+    ) external returns (bytes[] memory);
 }
