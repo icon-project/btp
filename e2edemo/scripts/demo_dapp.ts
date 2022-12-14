c5b542778d8c88e546dc884b9095911975c4f505
Jaechang Namgoong <jnamgoong@iconloop.com>
2022-12-14 17:20:41 +0900

    Refactor demo script to add more scenarios

--- e2edemo/scripts/demo_dapp.ts
+++ e2edemo/scripts/demo_dapp.ts
@@ -4,6 +4,7 @@ import {ethers} from 'hardhat';
 import {IconNetwork} from "./icon/network";
 import {Contract} from "./icon/contract";
 import {XCall} from "./icon/xcall";
+import {BigNumber} from "ethers";
 const {IconConverter} = IconService;
 const {E2E_DEMO_PATH} = process.env
 
@@ -45,67 +46,108 @@ function hexToString(data: string) {
   return msg;
 }
 
-async function sendMessageWithoutRollback() {
-  const icon = deployments.get('icon');
-  const hardhat = deployments.get('hardhat');
+function isIconChain(chain: any) {
+  return chain.network.indexOf('icon') != -1;
+}
 
-  const xcall = new XCall(iconNetwork, icon.contracts.xcall);
-  const fee = await xcall.getFee(hardhat.network, false);
-  console.log('fee=' + fee);
+function isHardhatChain(chain: any) {
+  return chain.network.indexOf('hardhat') != -1;
+}
 
-  const dapp = new DAppProxy(iconNetwork, icon.contracts.dapp);
-  const to = getBtpAddress(hardhat.network, hardhat.contracts.dapp);
-  const data = IconConverter.toHex('sendMessageWithoutRollback');
-  let sn;
-  console.log('[1] sendCallMessage: withoutRollback');
-  await dapp.sendMessage(to, data, fee)
-    .then((txHash) => dapp.getTxResult(txHash))
-    .then((result) => {
-      if (result.status != 1) {
-        throw new Error(`DApp: failed to sendMessage: ${result.txHash}`);
-      }
-      return dapp.filterEvent(result.eventLogs,
-        'CallMessageSent(Address,str,int,int,bytes)', xcall.address);
-    })
-    .then((event) => {
-      console.log(event);
-      sn = event.indexed ? parseInt(event.indexed[3], 16) : -1;
-    })
-  console.log(`serialNum=${sn}`);
+async function sendMessageFromDApp(srcChain: any, dstChain: any, msg: string) {
+  if (isIconChain(srcChain)) {
+    const xcallSrc = new XCall(iconNetwork, srcChain.contracts.xcall);
+    const fee = await xcallSrc.getFee(dstChain.network, false);
+    console.log('fee=' + fee);
 
-  console.log('wait some time for the message delivery...');
+    const dappSrc = new DAppProxy(iconNetwork, srcChain.contracts.dapp);
+    const to = getBtpAddress(dstChain.network, dstChain.contracts.dapp);
+    const data = IconConverter.toHex(msg);
+
+    let sn;
+    await dappSrc.sendMessage(to, data, fee)
+      .then((txHash) => dappSrc.getTxResult(txHash))
+      .then((result) => {
+        if (result.status != 1) {
+          throw new Error(`DApp: failed to sendMessage: ${result.txHash}`);
+        }
+        return dappSrc.filterEvent(result.eventLogs,
+          'CallMessageSent(Address,str,int,int,bytes)', xcallSrc.address);
+      })
+      .then((event) => {
+        console.log(event);
+        sn = event.indexed ? parseInt(event.indexed[3], 16) : -1;
+      })
+    console.log(`serialNum=${sn}`);
+  }
+}
+
+async function checkCallMessage(srcChain: any, dstChain: any) {
+  if (isHardhatChain(dstChain)) {
+    const xcallDst = await ethers.getContractAt('CallService', dstChain.contracts.xcall);
+    const filterCM = xcallDst.filters.CallMessage(
+      getBtpAddress(srcChain.network, srcChain.contracts.dapp),
+      dstChain.contracts.dapp
+    )
+    const logs = await xcallDst.queryFilter(filterCM, -5, "latest");
+    if (logs.length == 0) {
+      throw new Error(`DApp: could not find event: "CallMessage"`);
+    }
+    console.log(logs[0]);
+    return logs[0].args._reqId;
+  }
+  return BigNumber.from(-1)
+}
+
+async function invokeExecuteCall(dstChain: any, reqId: BigNumber) {
+  if (isHardhatChain(dstChain)) {
+    const xcallDst = await ethers.getContractAt('CallService', dstChain.contracts.xcall);
+    await xcallDst.executeCall(reqId)
+      .then((tx) => tx.wait(1));
+  }
+}
+
+async function verifyReceivedMessage(dstChain: any, msg: string) {
+  if (isHardhatChain(dstChain)) {
+    const dappDst = await ethers.getContractAt('DAppProxySample', dstChain.contracts.dapp);
+    const filterMR = dappDst.filters.MessageReceived();
+    const logs2 = await dappDst.queryFilter(filterMR, -5, "latest");
+    if (logs2.length == 0) {
+      throw new Error(`DApp: could not find event: "MessageReceived"`);
+    }
+    console.log(logs2)
+    const _from = logs2[0].args._from;
+    const _data = logs2[0].args._data;
+    const receivedMsg = hexToString(_data)
+    console.log(`From: ${_from}`);
+    console.log(`Data: ${_data}`);
+    console.log(`Msg: ${receivedMsg}`);
+    if (msg !== receivedMsg) {
+      throw new Error(`DApp: received message is different from the sent message`);
+    }
+  }
+}
+
+async function sendMessageWithoutRollback(src: string, dst: string) {
+  const srcChain = deployments.get(src);
+  const dstChain = deployments.get(dst);
+
+  const funcName = sendMessageWithoutRollback.name
+  console.log(`[1] ${funcName}: ${src} => ${dst}`);
+  const msgData = `${funcName}_${src}_${dst}`;
+  await sendMessageFromDApp(srcChain, dstChain, msgData);
+
+  console.log('[-] wait some time for the message delivery...');
   await sleep(3000);
 
-  console.log('[2] check CallMessage event on destination chain');
-  const xcallDst = await ethers.getContractAt('CallService', hardhat.contracts.xcall);
-  const filterCM = xcallDst.filters.CallMessage(
-    getBtpAddress(icon.network, icon.contracts.dapp),
-    hardhat.contracts.dapp
-  )
-  const logs = await xcallDst.queryFilter(filterCM, -10, "latest");
-  if (logs.length == 0) {
-    throw new Error(`DApp: could not find event: "CallMessage"`);
-  }
-  console.log(logs[0]);
-  const reqId = logs[0].args._reqId;
+  console.log(`[2] check CallMessage event on ${dst} chain`);
+  const reqId = await checkCallMessage(srcChain, dstChain);
 
   console.log(`[3] invoke executeCall with reqId=${reqId}`);
-  await xcallDst.executeCall(reqId)
-    .then((tx) => tx.wait(1));
+  await invokeExecuteCall(dstChain, reqId);
 
   console.log(`[4] verify the received message`);
-  const dappDst = await ethers.getContractAt('DAppProxySample', hardhat.contracts.dapp);
-  const filterMR = dappDst.filters.MessageReceived();
-  const logs2 = await dappDst.queryFilter(filterMR, -10, "latest");
-  if (logs2.length == 0) {
-    throw new Error(`DApp: could not find event: "MessageReceived"`);
-  }
-  console.log(logs2)
-  const _from = logs2[0].args._from;
-  const _data = logs2[0].args._data;
-  console.log(`From: ${_from}`);
-  console.log(`Data: ${_data}`);
-  console.log(`Msg: ${hexToString(_data)}`);
+  await verifyReceivedMessage(dstChain, msgData)
 }
 
 async function load_deployments() {
@@ -116,7 +158,7 @@ async function load_deployments() {
 }
 
 load_deployments()
-  .then(sendMessageWithoutRollback)
+  .then(() => sendMessageWithoutRollback('icon', 'hardhat'))
   .catch((error) => {
     console.error(error);
     process.exitCode = 1;
