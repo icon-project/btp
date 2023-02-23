4bb75673a57042994de3493ed74874c64dbecb2b
ByoungGul Kwon <kwon528@iconloop.com>
2023-02-23 17:49:36 +0900

    Implement changed interface in bridge

--- chain/icon/bridge/bridge.go
+++ chain/icon/bridge/bridge.go
@@ -34,20 +34,21 @@ func (r *receiveStatus) ReceiptProof() *ReceiptProof {
 }
 
 type bridge struct {
-	l   log.Logger
-	src types.BtpAddress
-	dst types.BtpAddress
-	c   *client.Client
-	nid int64
-	rsc chan link.ReceiveStatus
-	rss []*receiveStatus
+	l           log.Logger
+	src         types.BtpAddress
+	dst         types.BtpAddress
+	c           *client.Client
+	nid         int64
+	rsc         chan link.ReceiveStatus
+	rss         []*receiveStatus
+	startHeight int64
 }
 
-func newReceiveStatus(height, seq int64, offset int64, msgs []string, next types.BtpAddress) (*receiveStatus, error) {
+func newReceiveStatus(height, rxSeq int64, sn int64, msgs []string, next types.BtpAddress) (*receiveStatus, error) {
 	evts := make([]*Event, 0)
-	sn := offset + seq>>1
+	seq := sn
 	for _, msg := range msgs {
-		if sn > seq {
+		if sn > rxSeq {
 			evt, err := messageToEvent(next, msg, sn)
 			if err != nil {
 				return nil, err
@@ -68,7 +69,6 @@ func newReceiveStatus(height, seq int64, offset int64, msgs []string, next types
 		seq:    seq,
 		rp:     rp,
 	}, nil
-
 }
 
 func NewBridge(src, dst types.BtpAddress, endpoint string, l log.Logger) *bridge {
@@ -99,10 +99,15 @@ func (b *bridge) Start(bs *types.BMCLinkStatus) (<-chan link.ReceiveStatus, erro
 	if err := b.getNetworkId(); err != nil {
 		return nil, err
 	}
-	err := b.Monitoring(bs)
-	if err != nil {
+
+	if err := b.setStartHeight(); err != nil {
 		return nil, err
 	}
+
+	go func() {
+		b.Monitoring(bs)
+	}()
+
 	return b.rsc, nil
 }
 
@@ -115,69 +120,69 @@ func (b *bridge) GetStatus() (link.ReceiveStatus, error) {
 }
 
 func (b *bridge) GetHeightForSeq(seq int64) int64 {
-	return b.GetReceiveHeightForSequence(seq).height
+	rs := b.GetReceiveHeightForSequence(seq)
+	if rs != nil {
+		return b.GetReceiveHeightForSequence(seq).height
+	} else {
+		return 0
+	}
 }
 
-func (b *bridge) BuildBlockUpdate(bs *types.BMCLinkStatus, limit int64) ([]link.BlockUpdate, error) {
+func (b *bridge) BuildBlockUpdate(bls *types.BMCLinkStatus, limit int64) ([]link.BlockUpdate, error) {
+	b.updateReceiveStatus(bls)
+	bus := make([]link.BlockUpdate, 0)
+	for _, rs := range b.rss {
+		bu := NewBlockUpdate(bls, rs.Height())
+		bus = append(bus, bu)
+	}
+	return bus, nil
+}
+
+func (b *bridge) BuildBlockProof(bls *types.BMCLinkStatus, height int64) (link.BlockProof, error) {
 	return nil, nil
 }
 
-func (b *bridge) BuildBlockProof(bs *types.BMCLinkStatus, height int64) (link.BlockProof, error) {
-	return nil, nil
-}
-
-func (b *bridge) BuildMessageProof(bs *types.BMCLinkStatus, limit int64) (link.MessageProof, error) {
-	var offset int
+func (b *bridge) BuildMessageProof(bls *types.BMCLinkStatus, limit int64) (link.MessageProof, error) {
 	var rmSize int
-	rs := b.GetReceiveHeightForSequence(bs.RxSeq)
+	rs := b.GetReceiveHeightForSequence(bls.RxSeq + 1)
 	if rs == nil {
 		return nil, nil
 	}
-	offset = int(rs.seq - bs.RxSeq)
+	//offset := int64(rs.seq - bls.RxSeq)
+	messageCnt := len(rs.ReceiptProof().Events)
+	offset := bls.RxSeq - (rs.Seq() - int64(messageCnt))
 	trp := &ReceiptProof{
 		Index:  rs.ReceiptProof().Index,
 		Events: make([]*Event, 0),
 		Height: rs.Height(),
 	}
-	eventLen := len(rs.ReceiptProof().Events)
-	for i := offset; i < eventLen; i++ {
+
+	for i := offset; i < int64(messageCnt); i++ {
 		size := sizeOfEvent(rs.ReceiptProof().Events[i])
 		if limit < int64(rmSize+size) {
-			return NewMessageProof(bs.RxSeq, bs.RxSeq+int64(i), i, trp), nil
+			return NewMessageProof(bls, bls.RxSeq+i, trp)
 		}
 		trp.Events = append(trp.Events, rs.ReceiptProof().Events[i])
 		rmSize += size
 	}
 
 	//last event
-	return NewMessageProof(bs.RxSeq, bs.RxSeq+int64(eventLen), eventLen, trp), nil
+	return NewMessageProof(bls, bls.RxSeq+int64(messageCnt), trp)
 
 }
 
 func (b *bridge) BuildRelayMessage(rmis []link.RelayMessageItem) ([]byte, error) {
-	rm := &BridgeRelayMessage{
-		Receipts: make([][]byte, 0),
-	}
-	var (
-		rb  []byte
-		err error
-	)
-
+	//delete blockUpdate and only mp append
 	for _, rmi := range rmis {
-		r := &Receipt{
-			Index:  rmi.(*relayMessageItem).ReceiptProof().Index,
-			Events: rmi.(*relayMessageItem).Bytes(),
-			Height: rmi.(*relayMessageItem).ReceiptProof().Height,
+		if rmi.Type() == link.TypeMessageProof {
+			mp := rmi.(*MessageProof)
+			b.l.Debugf("BuildRelayMessage height:%d data:%s ", mp.nextBls.Verifier.Height,
+				base64.URLEncoding.EncodeToString(mp.Bytes()))
+
+			return mp.Bytes(), nil
 		}
-		if rb, err = codec.RLP.MarshalToBytes(r); err != nil {
-			return nil, err
-		}
-		rm.Receipts = append(rm.Receipts, rb)
 	}
-	if rb, err = codec.RLP.MarshalToBytes(rm); err != nil {
-		return nil, err
-	}
-	return rb, nil
+	return nil, nil
 }
 
 //TODO Refactoring reduplication func
@@ -211,20 +216,21 @@ func (b *bridge) Monitoring(bs *types.BMCLinkStatus) error {
 			bs.Verifier.Height, bs.TxSeq, b.nid, conn.LocalAddr().String())
 	}
 
-	err := b.monitorBTP2Block(req, onConn, onErr)
+	err := b.monitorBTP2Block(req, bs, onConn, onErr)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (b *bridge) monitorBTP2Block(req *client.BTPRequest, scb func(conn *websocket.Conn), errCb func(*websocket.Conn, error)) error {
+func (b *bridge) monitorBTP2Block(req *client.BTPRequest, bs *types.BMCLinkStatus, scb func(conn *websocket.Conn), errCb func(*websocket.Conn, error)) error {
 	offset, err := b.c.GetBTPLinkOffset(b.src, b.dst)
 	if err != nil {
 		return err
 	}
 	//BMC.seq starts with 1 and BTPBlock.FirstMessageSN starts with 0
 	offset += 1
+
 	return b.c.MonitorBTP(req, func(conn *websocket.Conn, v *client.BTPNotification) error {
 		h, err := base64.StdEncoding.DecodeString(v.Header)
 		if err != nil {
@@ -235,27 +241,31 @@ func (b *bridge) monitorBTP2Block(req *client.BTPRequest, scb func(conn *websock
 		if _, err = codec.RLP.UnmarshalFromBytes(h, bh); err != nil {
 			return err
 		}
-		msgs, err := b.getBtpMessage(bh.MainHeight)
-		if err != nil {
-			return err
-		}
+		if bh.MainHeight != b.startHeight {
+			msgs, err := b.getBtpMessage(bh.MainHeight)
+			if err != nil {
+				return err
+			}
+			sn := offset + bh.UpdateNumber>>1
 
-		rs, err := newReceiveStatus(bh.MainHeight, bh.UpdateNumber, offset, msgs, b.dst)
-		if err != nil {
-			return err
-		}
+			rs, err := newReceiveStatus(bh.MainHeight, bs.RxSeq, sn, msgs, b.dst)
+			if err != nil {
+				return err
+			}
+			b.rss = append(b.rss, rs)
+			b.l.Debugf("monitor info : Height:%d  UpdateNumber:%d  MessageCnt:%d ", bh.MainHeight, bh.UpdateNumber, len(msgs))
 
-		b.rss = append(b.rss, rs)
-		b.rsc <- rs
+			b.rsc <- rs
+		}
 		return nil
 	}, scb, errCb)
 }
 
 func (b *bridge) updateReceiveStatus(bs *types.BMCLinkStatus) {
 	for i, rs := range b.rss {
-		//TODO refactoring
-		if rs.Height() == bs.Verifier.Height && rs.Seq() == bs.RxSeq {
-			b.rss = b.rss[:i]
+		if rs.Height() <= bs.Verifier.Height && rs.Seq() <= bs.RxSeq {
+			b.rss = b.rss[i+1:]
+			return
 		}
 	}
 }
@@ -266,6 +276,17 @@ func (b *bridge) GetReceiveHeightForSequence(seq int64) *receiveStatus {
 			return rs
 		}
 	}
+	return nil
+}
+
+func (b *bridge) setStartHeight() error {
+	p := &client.BTPNetworkInfoParam{Id: client.HexInt(intconv.FormatInt(b.nid))}
+	ni, err := b.c.GetBTPNetworkInfo(p)
+	if err != nil {
+		return err
+	}
+	sh, err := ni.StartHeight.Value()
+	b.startHeight = sh + 1
 	return nil
 }
 
--- chain/icon/bridge/message.go
+++ chain/icon/bridge/message.go
@@ -30,8 +30,7 @@ type Event struct { //EventDataBTPMessage
 
 type relayMessageItem struct {
 	it      link.MessageItemType
-	rp      *ReceiptProof
-	bls     *types.BMCLinkStatus
+	nextBls *types.BMCLinkStatus
 	payload []byte
 }
 
@@ -47,12 +46,52 @@ func (c *relayMessageItem) Len() int64 {
 	return int64(len(c.payload))
 }
 
-func (c *relayMessageItem) UpdateBMCLinkStatus(status *types.BMCLinkStatus) error {
+func (c *relayMessageItem) UpdateBMCLinkStatus(bls *types.BMCLinkStatus) error {
+	bls.Verifier.Height = c.nextBls.Verifier.Height
+	bls.RxSeq = c.nextBls.RxSeq
+	bls.TxSeq = c.nextBls.TxSeq
 	return nil
 }
 
-func (c *relayMessageItem) ReceiptProof() *ReceiptProof {
-	return c.rp
+type blockUpdate struct {
+	blockProof
+	srcHeight    int64
+	targetHeight int64
+}
+
+func (c *blockUpdate) SrcHeight() int64 {
+	return c.srcHeight
+}
+
+func (c *blockUpdate) TargetHeight() int64 {
+	return c.targetHeight
+}
+
+type blockProof struct {
+	relayMessageItem
+	ph int64
+}
+
+func (c *blockProof) ProofHeight() int64 {
+	return c.ph
+}
+
+func NewBlockUpdate(bs *types.BMCLinkStatus, targetHeight int64) *blockUpdate {
+	nextBls := &types.BMCLinkStatus{}
+	nextBls.Verifier.Height = targetHeight
+	nextBls.TxSeq = bs.TxSeq
+	nextBls.RxSeq = bs.RxSeq
+	return &blockUpdate{
+		srcHeight:    bs.Verifier.Height,
+		targetHeight: targetHeight,
+		blockProof: blockProof{
+			relayMessageItem: relayMessageItem{
+				it:      link.TypeBlockUpdate,
+				nextBls: nextBls,
+			},
+			ph: targetHeight,
+		},
+	}
 }
 
 type MessageProof struct {
@@ -69,16 +108,50 @@ func (m *MessageProof) LastSeqNum() int64 {
 	return m.lastSeq
 }
 
-func NewMessageProof(ss, ls int64, pd int, rp *ReceiptProof) *MessageProof {
+func NewMessageProof(bs *types.BMCLinkStatus, ls int64, rp *ReceiptProof) (*MessageProof, error) {
+	//update bls
+	nextBls := &types.BMCLinkStatus{}
+	nextBls.Verifier.Height = bs.Verifier.Height
+	nextBls.TxSeq = bs.TxSeq
+	nextBls.RxSeq = ls
+
+	rm := &BridgeRelayMessage{
+		Receipts: make([][]byte, 0),
+	}
+
+	var (
+		b   []byte
+		err error
+	)
+	numOfEvents := 0
+	numOfEvents += len(rp.Events)
+	if b, err = codec.RLP.MarshalToBytes(rp.Events); err != nil {
+		return nil, err
+	}
+
+	r := &Receipt{
+		Index:  rp.Index,
+		Events: b,
+		Height: rp.Height,
+	}
+	if b, err = codec.RLP.MarshalToBytes(r); err != nil {
+		return nil, err
+	}
+	rm.Receipts = append(rm.Receipts, b)
+
+	if b, err = codec.RLP.MarshalToBytes(rm); err != nil {
+		return nil, err
+	}
+
 	return &MessageProof{
-		startSeq: ss,
+		startSeq: bs.RxSeq,
 		lastSeq:  ls,
 		relayMessageItem: relayMessageItem{
 			it:      link.TypeMessageProof,
-			payload: codec.RLP.MustMarshalToBytes(rp.Events),
-			rp:      rp,
+			payload: b,
+			nextBls: nextBls,
 		},
-	}
+	}, nil
 }
 
 type BridgeRelayMessage struct {
